04c59ed87c506c7e595204d96386c641f3cd42b0
Nicolas <nmarilleau@gmail.com>
2017-02-28 14:40:21 +0700

    architecture of the plugin

---
+++ femto-st.gama.mpi/src/femto/st/gama/mpi/IMPISkill.java
@@ -0,0 +1,11 @@
+package femto.st.gama.mpi;
+
+public interface IMPISkill {
+	String MPI_NETWORK = "MPI_Network";
+
+	
+	String MPI_RANK = "MPI_RANK";
+	
+	
+	
+}
--- femto-st.gama.mpi/src/femto_st/gama/mpi/MPISkill.java
+++
@@ -1,5 +0,0 @@
-package femto_st.gama.mpi;
-
-public class MPISkill {
-
-}
