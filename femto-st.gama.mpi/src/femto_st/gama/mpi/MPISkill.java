09d652ba986d46e668948165e7501241a5baec96
Nicolas <nmarilleau@gmail.com>
2017-03-02 17:09:09 +0100

    new version of the skill

--- femto-st.gama.mpi/src/femto_st/gama/mpi/MPISkill.java
+++ femto-st.gama.mpi/src/femto_st/gama/mpi/MPISkill.java
@@ -11,12 +11,13 @@ import msi.gama.precompiler.GamlAnnotations.example;
 import msi.gama.precompiler.GamlAnnotations.skill;
 import msi.gama.precompiler.GamlAnnotations.var;
 import msi.gama.precompiler.GamlAnnotations.vars;
+import msi.gaml.skills.Skill;
 import msi.gaml.types.IType;
 
 
 @vars({ @var(name = IMPISkill.MPI_RANK, type = IType.INT, doc = @doc("get MPI RANK"))})
 @skill(name=IMPISkill.MPI_NETWORK, concept = { IConcept.GUI, IConcept.COMMUNICATION, IConcept.SKILL })
-public class MPISkill {
+public class MPISkill extends Skill{
 	
 	@action(name = IMPISkill.MPI_RANK, args = {}, doc = @doc(value = "", returns = "", examples = { @example("") }))
 	public int getMPIRANK(IScope scope)
