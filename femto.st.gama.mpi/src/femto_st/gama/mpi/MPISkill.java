aeb6081ff777eff031f2f6b0a7b1603f65cb5a6a
Nicolas <nmarilleau@gmail.com>
2017-03-13 01:16:45 +0100

    first version of GAMA_MPI

--- femto.st.gama.mpi/src/femto_st/gama/mpi/MPISkill.java
+++ femto.st.gama.mpi/src/femto_st/gama/mpi/MPISkill.java
@@ -2,10 +2,10 @@ package femto_st.gama.mpi;
 
 import msi.gama.precompiler.IConcept;
 import msi.gama.runtime.IScope;
+
 import mpi.MPI;
 import mpi.MPIException;
 import msi.gama.precompiler.GamlAnnotations.action;
-import msi.gama.precompiler.GamlAnnotations.arg;
 import msi.gama.precompiler.GamlAnnotations.doc;
 import msi.gama.precompiler.GamlAnnotations.example;
 import msi.gama.precompiler.GamlAnnotations.skill;
@@ -15,24 +15,70 @@ import msi.gaml.skills.Skill;
 import msi.gaml.types.IType;
 
 
-@vars({ @var(name = IMPISkill.MPI_RANK, type = IType.INT, doc = @doc("get MPI RANK"))})
+@vars({ @var(name = IMPISkill.MPI_RANK, type = IType.INT, doc = @doc("Init MPI Brocker"))})
 @skill(name=IMPISkill.MPI_NETWORK, concept = { IConcept.GUI, IConcept.COMMUNICATION, IConcept.SKILL })
 public class MPISkill extends Skill{
+	static boolean isMPIInit = false;
+	
+	
+	@action(name = IMPISkill.MPI_INIT, args = {}, doc = @doc(value = "", returns = "", examples = { @example("") }))
+	public void mpiInit(IScope scope)
+	{
+		if(isMPIInit)
+			return;
+		
+		String[] arg={};
+		try {
+			System.out.println("INIT MPI ");
+			MPI.Init(arg);
+		} catch (MPIException e) {
+			System.out.println("MIP Init Error" + e);
+		}
+	}
 	
 	@action(name = IMPISkill.MPI_RANK, args = {}, doc = @doc(value = "", returns = "", examples = { @example("") }))
 	public int getMPIRANK(IScope scope)
 	{
 		  int rank = 2;
 		    try {
-
-			rank = MPI.COMM_WORLD.getRank();
+		    rank = MPI.COMM_WORLD.getRank();
 
 		    } catch (MPIException mpiex) {
-
-			System.out.println("MIP Error");
+		    	System.out.println("MIP Error"+mpiex);
 		    }
 		
 		return rank;
 	}
+	
+	
+	private void initialize(final IScope scope) {
+		isMPIInit = true;
+	}
+
+	private void startSkill(final IScope scope) {
+		initialize(scope);
+		registerSimulationEvent(scope);
+	}
+	
+	private void finalizeMPI(final IScope scope)
+	{
+		try {
+			MPI.Finalize();
+			isMPIInit = false;
+		} catch (MPIException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
+	private void registerSimulationEvent(final IScope scope) {
+			scope.getSimulation().postDisposeAction(scope1 -> {
+				finalizeMPI(scope1);
+			return null;
+		});
+	}
+
+	
 	
 }
--- femto.st.gama.mpi/src/femto_st/gama/mpi/runtime/Startup.java
+++ femto.st.gama.mpi/src/femto_st/gama/mpi/runtime/Startup.java
@@ -1,23 +1,9 @@
 package femto_st.gama.mpi.runtime;
 
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
 
 import mpi.MPI;
 
-public class Startup implements BundleActivator {
+public class Startup {
 
-	@Override
-	public void start(BundleContext context) throws Exception {
-		String[] args= {};
-		MPI.Init(args);
-		
-	}
-
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		MPI.Finalize();
-		
-	}
 
 }
