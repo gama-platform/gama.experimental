d0e71ac1e5a867a6d9e7c643341cbe6cfeea7457
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-03-22 14:35:27 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,2 @@
-cd ../ummisco.gama.annotations &&
-mvn clean install &&
-cd - &&
-cd ../msi.gama.processor &&
-mvn clean install &&
-cd - && cd msi.gama.experimental.parent &&
-mvn clean install
+cd msi.gama.experimental.parent &&
+mvn clean install && cd - 
