76240c646de51ac03013d2b342b1ae3d567ed271
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-03-22 14:33:03 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-cd cict.gaml.extensions.netcdf && mvn clean install && cd ../msi.gama.experimental.parent &&
+cd ../ummisco.gama.annotations &&
+mvn clean install &&
+cd - &&
+cd ../msi.gama.processor &&
+mvn clean install &&
+cd - && cd msi.gama.experimental.parent &&
 mvn clean install
