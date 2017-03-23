e7af894d6ae6a26f6abc0d5e80ef5bea26f5f895
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-03-23 11:01:25 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
+travis encrypt -r hqnghi88/gamaExpClone "CI_DEPLOY_USERNAME=hqnghi88" &&
+travis encrypt -r hqnghi88/gamaExpClone "CI_DEPLOY_PASSWORD=23111988" &&
 cd msi.gama.experimental.parent &&
 mvn clean install && cd - 
