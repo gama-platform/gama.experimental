9d53ea56ab13bd65d17fbaffc434a4e9c1fbb480
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-03-24 11:41:39 +0700

    b

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd msi.gama.experimental.p2updatesite &&
-mvn clean install -P p2Repo --settings ../settings.xml && 
+mvn clean deploy --settings ../settings.xml -DskipTests=true -B  && 
 cd -
