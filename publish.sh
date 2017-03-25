f9e5add40beb5d4e1cab465006e7704a35048831
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-03-26 02:24:24 +0700

    Working with Travis CI, sorry for the inconvenience!!

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd msi.gama.experimental.p2updatesite &&
-mvn clean install -P p2Repo --settings ../settings.xml && 
+mvn -U clean install -P p2Repo --settings ../settings.xml && 
 cd -
