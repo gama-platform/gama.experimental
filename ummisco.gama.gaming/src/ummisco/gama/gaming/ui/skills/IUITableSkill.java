893b71fa76e1dbc03979a6fc69fe28f071e6ba10
Nicolas <nmarilleau@gmail.com>
2017-03-27 15:37:12 +0200

    add new files

---
+++ ummisco.gama.gaming/src/ummisco/gama/gaming/ui/skills/IUITableSkill.java
@@ -0,0 +1,18 @@
+package ummisco.gama.gaming.ui.skills;
+
+
+public interface IUITableSkill  {
+	public static String SKILL_NAME = "UI_table";
+	public static String NUMBER_OF_COLUMNS = "number_of_columns";
+	public static String NUMBER_OF_LINES = "number_of_lines";
+	public static String MATRIX_COLUMN = "column";
+	public static String MATRIX_LINE = "line";
+	public static String MATRIX_DATA = "matrix_data";
+	
+	public static String ADD_LINE = "add_line";
+	public static String ELEMENTS_TO_ADD = "elements";
+	
+	
+	
+	
+}
