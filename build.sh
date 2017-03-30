7ceec759a4048e32997b4632b1724993bd1c445c
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-03-30 09:39:35 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,40 @@
+#!/bin/bash
+git_push(){
+    echo "git push new change of parent pom"
+    git config --global user.email "travis@travis-ci.com"
+    git config --global user.name "hqnghi88"
+    git config --global push.default simple
+    git remote rm origin
+    git remote add origin https://hqnghi88:$HQN_KEY@github.com/hqnghi88/gamaExpClone.git
+    git add -A
+    git commit -m "[ci skip] generate parent pom"
+    git push origin HEAD:master
+}
+
+generate_parent_pom(){
+    header=$(<msi.gama.experimental.parent/pom_header.xml)
+    current_modules=$(<msi.gama.experimental.parent/pom_modules.xml)
+    footer=$(<msi.gama.experimental.parent/pom_footer.xml)
+
+    modules=$'\n'$"<modules>"$'\n'
+    for file in *; do 
+      if [[ -d "$file" && ! -L "$file" ]]; then
+        echo "$file is a directory"; 
+        if [ -f "$file/pom.xml" ]; then
+            echo "File $file/pom.xml found!"        
+            modules="$modules <module>../$file</module> "$'\n'
+        fi
+      fi; 
+    done
+
+    modules="$modules </modules>"$'\n'
+    if [[ "$current_modules" != "$modules" ]]; then
+        echo "$modules" > msi.gama.experimental.parent/pom_modules.xml
+        echo " $header $modules $footer " > msi.gama.experimental.parent/pom.xml
+        git_push
+    fi
+}
+generate_parent_pom
 cd msi.gama.experimental.parent &&
 mvn clean install &&
 cd -
