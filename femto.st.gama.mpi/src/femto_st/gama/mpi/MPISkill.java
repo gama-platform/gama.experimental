5640e4ee5a0f42c2399597783e3b7ae15f5a1cba
lphilippe <laurent.philippe@univ-fcomte.fr>
2017-05-29 14:43:08 +0200

    enhaced MPI skill

--- femto.st.gama.mpi/src/femto_st/gama/mpi/MPISkill.java
+++ femto.st.gama.mpi/src/femto_st/gama/mpi/MPISkill.java
@@ -3,9 +3,9 @@ package femto_st.gama.mpi;
 import msi.gama.precompiler.IConcept;
 import msi.gama.runtime.IScope;
 
-import mpi.MPI;
-import mpi.MPIException;
+import mpi.*;
 import msi.gama.precompiler.GamlAnnotations.action;
+import msi.gama.precompiler.GamlAnnotations.arg;
 import msi.gama.precompiler.GamlAnnotations.doc;
 import msi.gama.precompiler.GamlAnnotations.example;
 import msi.gama.precompiler.GamlAnnotations.skill;
@@ -14,12 +14,20 @@ import msi.gama.precompiler.GamlAnnotations.vars;
 import msi.gaml.skills.Skill;
 import msi.gaml.types.IType;
 
-
 @vars({ @var(name = IMPISkill.MPI_RANK, type = IType.INT, doc = @doc("Init MPI Brocker"))})
 @skill(name=IMPISkill.MPI_NETWORK, concept = { IConcept.GUI, IConcept.COMMUNICATION, IConcept.SKILL })
 public class MPISkill extends Skill{
 	static boolean isMPIInit = false;
 	
+	
+	private void initialize(final IScope scope) {
+		isMPIInit = true;
+	}
+	
+	private void startSkill(final IScope scope) {
+		initialize(scope);
+		registerSimulationEvent(scope);
+	}
 	
 	@action(name = IMPISkill.MPI_INIT, args = {}, doc = @doc(value = "", returns = "", examples = { @example("") }))
 	public void mpiInit(IScope scope)
@@ -32,8 +40,22 @@ public class MPISkill extends Skill{
 			System.out.println("INIT MPI ");
 			MPI.Init(arg);
 		} catch (MPIException e) {
-			System.out.println("MIP Init Error" + e);
+			System.out.println("MPI Init Error" + e);
 		}
+	}
+	
+	@action(name = IMPISkill.MPI_SIZE, args = {}, doc = @doc(value = "", returns = "", examples = { @example("") }))
+	public int getMPISIZE(IScope scope)
+	{
+		  int size = 2;
+		    try {
+		    size = MPI.COMM_WORLD.getSize();
+
+		    } catch (MPIException mpiex) {
+		    	System.out.println("MPI Size Error"+mpiex);
+		    }
+		
+		return size;
 	}
 	
 	@action(name = IMPISkill.MPI_RANK, args = {}, doc = @doc(value = "", returns = "", examples = { @example("") }))
@@ -44,22 +66,52 @@ public class MPISkill extends Skill{
 		    rank = MPI.COMM_WORLD.getRank();
 
 		    } catch (MPIException mpiex) {
-		    	System.out.println("MIP Error"+mpiex);
+		    	System.out.println("MPI rank Error"+mpiex);
 		    }
 		
 		return rank;
 	}
 	
-	
-	private void initialize(final IScope scope) {
-		isMPIInit = true;
-	}
+	@action(name = IMPISkill.MPI_SEND, args = {
+			@arg(name = IMPISkill.DEST, type = IType.INT, doc = @doc("dest destinataire")),
+			@arg(name = IMPISkill.STAG, type = IType.INT, doc = @doc("stag message tag"))
+	}, doc = @doc(value = "", returns = "", examples = { @example("") }))
+	public void send(IScope scope)
+	{
+		int dest = ((Integer) scope.getArg(IMPISkill.DEST, IType.INT)).intValue();
+		int stag = ((Integer) scope.getArg(IMPISkill.STAG, IType.INT)).intValue();
+		
+		int message[] = new int [1];
+		message[0] = 14;
+		try {
+		    MPI.COMM_WORLD.send( message, 1, MPI.INT, dest, stag);
 
-	private void startSkill(final IScope scope) {
-		initialize(scope);
-		registerSimulationEvent(scope);
+	    } catch (MPIException mpiex) {
+	    	System.out.println("MPI send Error"+mpiex);
+	    }
 	}
 	
+	@action(name = IMPISkill.MPI_RECV, args = {
+			@arg(name = IMPISkill.SOURCE, type = IType.INT, doc = @doc("source sender")),
+			@arg(name = IMPISkill.RTAG, type = IType.INT, doc = @doc("rtag message tag"))
+	}, doc = @doc(value = "", returns = "", examples = { @example("") }))
+	public int recv(IScope scope)
+	{
+		int source = ((Integer) scope.getArg(IMPISkill.SOURCE, IType.INT)).intValue();
+		int rtag = ((Integer) scope.getArg(IMPISkill.RTAG, IType.INT)).intValue();
+		
+		int message[] = new int [1];
+		try {
+		    MPI.COMM_WORLD.recv( message, 1, MPI.INT, source, rtag);
+		    System.out.println("message = " + message[0]);
+	    } catch (MPIException mpiex) {
+	    	System.out.println("MPI send Error"+mpiex);
+	    }
+		
+		return message[0];
+	}
+	
+		
 	private void finalizeMPI(final IScope scope)
 	{
 		try {
@@ -71,7 +123,6 @@ public class MPISkill extends Skill{
 		}
 	}
 
-	
 	private void registerSimulationEvent(final IScope scope) {
 			scope.getSimulation().postDisposeAction(scope1 -> {
 				finalizeMPI(scope1);
