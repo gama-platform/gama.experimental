d4f9685874d1fe43b60cea9efe4e9cb318b9ca9b
Damien Philippon <damien.philippon.dev@gmail.com>
2017-12-20 02:21:17 +0700

    Refactoring documentation (Use of ISyntacticElement)
    Now possible to call the documentation of a project or a model without
    being in the navigator. However, calling the documentation on a model in
    the navigator will create the full documentation of the model, whereas
    it is not the case with the call not being in the navigator (just
    refreshing the model documentation)

---
+++ ummisco.gama.markdown.documentation/src/jobs/JobDocumentationModelIndependent.java
@@ -0,0 +1,137 @@
+package jobs;
+
+import java.io.File;
+import java.util.Iterator;
+
+import org.apache.commons.io.FilenameUtils;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.URI;
+
+import markdownSyntactic.LightModel;
+import markdownSyntactic.MarkdownModelDocumentor;
+import msi.gama.lang.gaml.indexer.GamlResourceIndexer;
+import ummisco.gama.ui.navigator.contents.ResourceManager;
+import ummisco.gama.ui.navigator.contents.WrappedGamaFile;
+/**
+ * 
+ * @author damienphilippon
+ * Date : 19 Dec 2017
+ * Class used to do a Documentation job for an Independent Model (build the model, its index and its imports documentation)
+ */
+public class JobDocumentationModelIndependent extends JobDocumentation {
+
+	/**
+	 * The file of the model
+	 */
+	public WrappedGamaFile file;
+
+	/**
+	 * Constructor using a given model file and a given output directory
+	 * @param aFile {@code WrappedGamaFile} the file containing the model
+	 * @param directory {@code String} the string representing the path for the outputs
+	 */
+	public JobDocumentationModelIndependent(WrappedGamaFile aFile, String directory) {
+		super(directory);
+		this.file = aFile;
+	}
+	
+	/**
+	 * Method to run the job in background
+	 */
+	public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException {
+		generateForWrappedGamaFile(this.file);
+		generateIndex();
+		return Status.OK_STATUS;
+	}
+	/**
+	 * Overriding the method to build the documentatioon of the imported models also
+	 */
+	public void generateForWrappedGamaFile(WrappedGamaFile aFile)
+	{ 
+		//Build the documentation of the model first
+		IPath pathProject = aFile.getProject().getResource().getLocation();
+		IPath pathResource = aFile.getResource().getLocation();
+		String pathResourceToProject = FilenameUtils.removeExtension(pathResource.makeRelativeTo(pathProject).toOSString());
+		(new File(directory+File.separator+pathResourceToProject+".md")).getParentFile().mkdirs();
+		MarkdownModelDocumentor modelDoc = new MarkdownModelDocumentor(aFile,directory+File.separator+pathResourceToProject+".md");
+		modelDoc.generateMarkdown();
+		modelDoc.saveMarkdown();
+		
+		
+		//Add the links to the different species and experiments contained in the model
+		LightModel model = new LightModel(URI.createURI(aFile.getResource().getLocationURI().toString()));
+		if(modelsDone.contains(aFile.getResource().getLocationURI().toString())==false)
+		{
+			modelsDone.add(aFile.getResource().getLocationURI().toString());
+			IPath importedModel = aFile.getResource().getLocation();
+			IPath relativisedPath = importedModel.makeRelativeTo(new Path(indexPath));
+			for(String aSpecies : model.speciesLink.keySet())
+			{
+				String linkToSpecies = model.speciesLink.get(aSpecies);
+				linkToSpecies=FilenameUtils.removeExtension(relativisedPath.toString().replaceFirst(".."+File.separator, "").replaceFirst(".."+File.separator, ""))+".md#"+linkToSpecies;
+				this.speciesLink.put(aSpecies, linkToSpecies);
+			}
+			for(String anExperiment : model.experimentsLink.keySet())
+			{
+				String linkToExperiment = model.experimentsLink.get(anExperiment);
+				linkToExperiment=FilenameUtils.removeExtension(relativisedPath.toString().replaceFirst(".."+File.separator,"").replaceFirst(".."+File.separator, ""))+".md#"+linkToExperiment;
+				this.experimentsLink.put(anExperiment, linkToExperiment);
+			}
+		}
+		
+		//Generate the documentation for the imported models
+		
+		Iterator<URI> importedUris = GamlResourceIndexer.allImportsOf(URI.createURI(aFile.getResource().getLocationURI().toString()));
+		while(importedUris.hasNext())
+		{
+			URI tmpUri = importedUris.next();
+			//Compute the relative path
+			IPath importedModelPath = new Path(tmpUri.toFileString().replace("file:", ""));
+			IPath relativisedPath = importedModelPath.makeRelativeTo(new Path(indexPath));
+			IFile tmpfile = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(importedModelPath); 
+			WrappedGamaFile importedModel = (WrappedGamaFile) ResourceManager.getInstance().findWrappedInstanceOf(tmpfile);
+			
+			LightModel lightModelImported = new LightModel(tmpUri);
+			
+			//If the model has not been documented yet by the job, add the links to its species and experiments
+			if(modelsDone.contains(importedModel.getResource().getLocationURI().toString())==false)
+			{
+				modelsDone.add(importedModel.getResource().getLocationURI().toString());
+				IPath importedLightModelPath = importedModel.getResource().getLocation();
+				IPath relativisedLightModelPath = importedLightModelPath.makeRelativeTo(new Path(indexPath));
+				for(String aSpecies : lightModelImported.speciesLink.keySet())
+				{
+					String linkToSpecies = model.speciesLink.get(aSpecies);
+					linkToSpecies=FilenameUtils.removeExtension(relativisedLightModelPath.toString().replaceFirst(".."+File.separator, "").replaceFirst(".."+File.separator, ""))+".md#"+linkToSpecies;
+					this.speciesLink.put(aSpecies, linkToSpecies);
+				}
+				for(String anExperiment : lightModelImported.experimentsLink.keySet())
+				{
+					String linkToExperiment = model.experimentsLink.get(anExperiment);
+					linkToExperiment=FilenameUtils.removeExtension(relativisedLightModelPath.toString().replaceFirst(".."+File.separator,"").replaceFirst(".."+File.separator, ""))+".md#"+linkToExperiment;
+					this.experimentsLink.put(anExperiment, linkToExperiment);
+				}
+			}
+			String pathResourceModelToProject = FilenameUtils.removeExtension(relativisedPath.toOSString().replaceFirst(".."+File.separator, "").replaceFirst(".."+File.separator, ""));
+			(new File(directory+File.separator+pathResourceModelToProject+".md")).getParentFile().mkdirs();
+			
+			//Generate the documentation of the imported models
+			MarkdownModelDocumentor importedModelDoc = new MarkdownModelDocumentor(importedModel,directory+File.separator+pathResourceModelToProject+".md");
+			importedModelDoc.generateMarkdown();
+			importedModelDoc.saveMarkdown();
+			
+		}
+		//Release the memory used by the variables
+		importedUris=null;
+		pathProject=null;
+		pathResource=null;
+		pathResourceToProject=null;
+	}
+}
---
+++ ummisco.gama.markdown.documentation/src/markdownSyntactic/LightModel.java
@@ -0,0 +1,73 @@
+package markdownSyntactic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.URI;
+
+import msi.gama.util.GAML;
+import msi.gaml.compilation.ast.ISyntacticElement;
+import msi.gaml.compilation.ast.ISyntacticElement.SyntacticVisitor;
+/**
+ * 
+ * @author damienphilippon
+ * Date : 19 Dec 2017
+ * Class used to have few information on a model without using too much memory
+ */
+public class LightModel {
+	/**
+	 * ISyntacticElement representing the model
+	 */
+	public ISyntacticElement modelElement;
+	
+	/**
+	 * Variable representing all the species and the link to the documentation files that will present them
+	 */
+	public Map<String, String> speciesLink = new HashMap<String,String>();
+	/**
+	 * Variable representing all the experiments and the link to the documentation files that will present them
+	 */
+	public Map<String, String> experimentsLink = new HashMap<String,String>();
+	
+	/**
+	 * Visitor of the model to load the species and experiment in the link maps
+	 */
+	SyntacticVisitor visitorForLoading = new SyntacticVisitor()
+	{
+
+		@Override
+		public void visit(ISyntacticElement element) {
+			if((element.isSpecies())||(element.getKeyword().equals(IParser.GAMA_KEYWORD_GRID)))
+			{
+				speciesLink.put(element.getName(), element.getKeyword()+"-"+element.getName());
+			}
+			if(element.isExperiment())
+			{
+				experimentsLink.put(element.getName(),element.getKeyword()+"-"+element.getName()+"-"+element.getExpressionAt(IParser.GAMA_KEYWORD_TYPE)+"-");
+			}
+		}
+	};
+	
+	/**
+	 * Constructor of LightModel using an URI to get the contents from that URI
+	 * @param aUri {@code URI} the URI used to get the contents
+	 */
+	public LightModel(URI aUri)
+	{
+		this.modelElement=GAML.getContents(aUri);
+		this.modelElement.visitSpecies(visitorForLoading);
+		this.modelElement.visitGrids(visitorForLoading);
+		this.modelElement.visitExperiments(visitorForLoading);
+	}
+	
+	/**
+	 * Method to release the memory used by the different variables of the object Light Model
+	 */
+	public void dispose()
+	{
+		speciesLink=null;
+		experimentsLink=null;
+		modelElement=null;
+	}
+	
+}
