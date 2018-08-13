a3879002fef97c8489460192209a7d1bb2b295c3
AlexisDrogoul <alexis.drogoul@gmail.com>
2018-08-13 11:39:10 +0700

    Fixing deprecated API calls

--- mit.gaml.extension.launchpad/src/mit/gaml/extension/launchpad/skills/LaunchPadEventLayer.java
+++ mit.gaml.extension.launchpad/src/mit/gaml/extension/launchpad/skills/LaunchPadEventLayer.java
@@ -1,7 +1,6 @@
 /*********************************************************************************************
  *
- * 'EventLayer.java, in plugin msi.gama.core, is part of the source code of the
- * GAMA modeling and simulation platform.
+ * 'EventLayer.java, in plugin msi.gama.core, is part of the source code of the GAMA modeling and simulation platform.
  * (c) 2007-2016 UMI 209 UMMISCO IRD/UPMC & Partners
  *
  * Visit https://github.com/gama-platform/gama for license information and developers contact.
@@ -11,13 +10,13 @@
 package mit.gaml.extension.launchpad.skills;
 
 import java.io.IOException;
+
 import msi.gama.common.interfaces.IDisplaySurface;
 import msi.gama.common.interfaces.IEventLayerDelegate;
 import msi.gama.common.interfaces.IGraphics;
 import msi.gama.metamodel.agent.IAgent;
 import msi.gama.outputs.layers.AbstractLayer;
 import msi.gama.outputs.layers.EventLayerStatement;
-import msi.gama.outputs.layers.IDisplayLayerBox;
 import msi.gama.outputs.layers.ILayerStatement;
 import msi.gama.runtime.IScope;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
@@ -27,7 +26,6 @@ import net.thecodersbreakfast.lp4j.api.Button;
 import net.thecodersbreakfast.lp4j.api.Color;
 import net.thecodersbreakfast.lp4j.api.Launchpad;
 import net.thecodersbreakfast.lp4j.api.LaunchpadClient;
-import net.thecodersbreakfast.lp4j.api.LaunchpadListener;
 import net.thecodersbreakfast.lp4j.api.LaunchpadListenerAdapter;
 import net.thecodersbreakfast.lp4j.api.Pad;
 import net.thecodersbreakfast.lp4j.midi.MidiDeviceConfiguration;
@@ -37,19 +35,11 @@ import net.thecodersbreakfast.lp4j.midi.MidiLaunchpad;
  * Written by Arnaud Grignard & Huynh Quang Nghi (CC2017)
  */
 
-public class LaunchPadEventLayer extends AbstractLayer implements IEventLayerDelegate  {
-
-	@Override
-	protected void setPositionAndSize(final IDisplayLayerBox box, final IGraphics g) {
-		super.setPositionAndSize(box, g);
-	}
+public class LaunchPadEventLayer extends AbstractLayer implements IEventLayerDelegate {
 
 	MyLPListener myListener;
 	IScope executionScope;
-	
-	public LaunchPadEventLayer() {		
-	}
-	
+
 	public LaunchPadEventLayer(final ILayerStatement layer) {
 		super(layer);
 	}
@@ -59,21 +49,22 @@ public class LaunchPadEventLayer extends AbstractLayer implements IEventLayerDel
 		super.disableOn(surface);
 	}
 
-    public static boolean launch=false;
-	Launchpad launchpad ;
+	public static boolean launch = false;
+	Launchpad launchpad;
 	public static LaunchpadClient client;
+
 	@Override
 	public void firstLaunchOn(final IDisplaySurface surface) {
 		super.firstLaunchOn(surface);
-		launch=true;
+		launch = true;
 		try {
 			System.out.println("LaunchPad launched");
 			launchpad = new MidiLaunchpad(MidiDeviceConfiguration.autodetect());
 			client = launchpad.getClient();
 			myListener = new MyLPListener(client);
-			launchpad.setListener((LaunchpadListener) myListener);
+			launchpad.setListener(myListener);
 			client.reset();
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -82,10 +73,10 @@ public class LaunchPadEventLayer extends AbstractLayer implements IEventLayerDel
 	@Override
 	public void dispose() {
 		super.dispose();
-        try {
-        	client.reset();
+		try {
+			client.reset();
 			launchpad.close();
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -96,82 +87,75 @@ public class LaunchPadEventLayer extends AbstractLayer implements IEventLayerDel
 		return "Event layer";
 	}
 
-
 	private void executeEvent() {
-		final IAgent agent = ((EventLayerStatement) definition).executesInSimulation()
-				? executionScope.getSimulation() : executionScope.getExperiment();
-				String actionName= ((EventLayerStatement) definition).getFacet("action").toString();
+		final IAgent agent = ((EventLayerStatement) definition).executesInSimulation() ? executionScope.getSimulation()
+				: executionScope.getExperiment();
+		final String actionName = ((EventLayerStatement) definition).getFacet("action").toString();
 		final IExecutable executer = agent == null ? null : agent.getSpecies().getAction(actionName);
-		if (executer == null) {
-			return;
-		}
+		if (executer == null) { return; }
 		executionScope.execute(executer, agent, null);
-		//GAMA.getExperiment().refreshAllOutputs();
+		// GAMA.getExperiment().refreshAllOutputs();
 	}
 
 	public static Pad pressedPad;
 	public static Button pressedButton;
-    public class MyLPListener extends LaunchpadListenerAdapter {
 
-        private final LaunchpadClient client;
+	public class MyLPListener extends LaunchpadListenerAdapter {
 
-        public MyLPListener(LaunchpadClient client) {
-            this.client = client;
-        }
+		private final LaunchpadClient client;
 
-        @Override
-        public void onPadPressed(Pad pad, long timestamp) {
-            client.setPadLight(pad, Color.YELLOW, BackBufferOperation.NONE);
-            pressedPad=pad;
-        }
+		public MyLPListener(final LaunchpadClient client) {
+			this.client = client;
+		}
 
-        @Override
-        public void onPadReleased(Pad pad, long timestamp) {
-            client.setPadLight(pad, Color.BLACK, BackBufferOperation.NONE);
-            pressedPad=pad;
-            executeEvent();
-        }
-        
-        @Override
-        public void onButtonPressed(Button button, long timestamp) {
-            client.setButtonLight(button, Color.YELLOW, BackBufferOperation.NONE);
-            pressedButton=button;
-        }
-        @Override
-        public void onButtonReleased(Button button, long timestamp) {
-            client.setButtonLight(button, Color.BLACK, BackBufferOperation.NONE);
-            pressedButton=button;
-        }
-    }
+		@Override
+		public void onPadPressed(final Pad pad, final long timestamp) {
+			client.setPadLight(pad, Color.YELLOW, BackBufferOperation.NONE);
+			pressedPad = pad;
+		}
 
-	@Override
-	protected void privateDrawDisplay(final IScope scope, final IGraphics g) throws GamaRuntimeException {
-	}
+		@Override
+		public void onPadReleased(final Pad pad, final long timestamp) {
+			client.setPadLight(pad, Color.BLACK, BackBufferOperation.NONE);
+			pressedPad = pad;
+			executeEvent();
+		}
 
-	@Override
-	public void drawDisplay(final IScope scope, final IGraphics g) throws GamaRuntimeException {
-		if (definition != null) {
-			definition.getBox().compute(scope);
-			setPositionAndSize(definition.getBox(), g);
+		@Override
+		public void onButtonPressed(final Button button, final long timestamp) {
+			client.setButtonLight(button, Color.YELLOW, BackBufferOperation.NONE);
+			pressedButton = button;
+		}
+
+		@Override
+		public void onButtonReleased(final Button button, final long timestamp) {
+			client.setButtonLight(button, Color.BLACK, BackBufferOperation.NONE);
+			pressedButton = button;
 		}
 	}
 
 	@Override
-	public boolean acceptSource(IScope scope, Object source) {
+	protected void privateDraw(final IScope scope, final IGraphics g) throws GamaRuntimeException {}
+
+	@Override
+	public void draw(final IScope scope, final IGraphics g) throws GamaRuntimeException {
+		getData().compute(scope, g);
+	}
+
+	@Override
+	public boolean acceptSource(final IScope scope, final Object source) {
 		// TODO Auto-generated method stub
-		if(source.equals("launchpad")) {
-			return true;
-		}
+		if (source.equals("launchpad")) { return true; }
 		return false;
 	}
 
 	@Override
-	public boolean createFrom(IScope scope, Object source, EventLayerStatement statement) {
+	public boolean createFrom(final IScope scope, final Object source, final EventLayerStatement statement) {
 		// TODO Auto-generated method stub
 		executionScope = scope;
-		System.out.println("LAUNCHPAD event layer delegate "+statement.getFacetValue(scope, "action"));
-		if(!launch) {			
-			definition=statement;
+		System.out.println("LAUNCHPAD event layer delegate " + statement.getFacetValue(scope, "action"));
+		if (!launch) {
+			definition = statement;
 			firstLaunchOn(null);
 		}
 		return false;
--- mit.gaml.extension.launchpad/src/mit/gaml/extension/launchpad/skills/LaunchPadSkill.java
+++ mit.gaml.extension.launchpad/src/mit/gaml/extension/launchpad/skills/LaunchPadSkill.java
@@ -1,85 +1,66 @@
 package mit.gaml.extension.launchpad.skills;
 
-import msi.gama.common.interfaces.IKeyword;
-import msi.gama.metamodel.agent.IAgent;
 import msi.gama.metamodel.shape.GamaPoint;
 import msi.gama.metamodel.shape.ILocation;
-import msi.gama.metamodel.shape.IShape;
-import msi.gama.precompiler.IConcept;
 import msi.gama.precompiler.GamlAnnotations.action;
 import msi.gama.precompiler.GamlAnnotations.arg;
 import msi.gama.precompiler.GamlAnnotations.doc;
 import msi.gama.precompiler.GamlAnnotations.example;
 import msi.gama.precompiler.GamlAnnotations.getter;
-import msi.gama.precompiler.GamlAnnotations.setter;
 import msi.gama.precompiler.GamlAnnotations.skill;
-import msi.gama.precompiler.GamlAnnotations.var;
+import msi.gama.precompiler.GamlAnnotations.variable;
 import msi.gama.precompiler.GamlAnnotations.vars;
+import msi.gama.precompiler.IConcept;
 import msi.gama.runtime.IScope;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
-import msi.gama.util.GamaColor;
-import msi.gama.util.path.IPath;
-import msi.gaml.operators.Maths;
 import msi.gaml.skills.Skill;
-import msi.gaml.types.GamaGeometryType;
 import msi.gaml.types.IType;
 import net.thecodersbreakfast.lp4j.api.BackBufferOperation;
 import net.thecodersbreakfast.lp4j.api.Color;
-import net.thecodersbreakfast.lp4j.api.Launchpad;
-import net.thecodersbreakfast.lp4j.api.LaunchpadClient;
-import net.thecodersbreakfast.lp4j.api.LaunchpadListener;
-import net.thecodersbreakfast.lp4j.midi.MidiDeviceConfiguration;
-import net.thecodersbreakfast.lp4j.midi.MidiLaunchpad;
 
-
-
-@vars ({ @var (
+@vars ({ @variable (
 		name = "buttonPressed",
 		type = IType.STRING,
 		doc = @doc ("get button pressed name")),
-		@var (
-			name = "padPressed",
-			type = IType.POINT,
-			doc = @doc ("get pad pressed name"))})
-
+		@variable (
+				name = "padPressed",
+				type = IType.POINT,
+				doc = @doc ("get pad pressed name")) })
 
 @doc ("The launchpad skill is intended to interact with a simulation using a launchpad interface")
-@skill(name = "launchpadskill", concept = { IConcept.LAYER })
-public class LaunchPadSkill  extends Skill{
-		
+@skill (
+		name = "launchpadskill",
+		concept = { IConcept.LAYER })
+public class LaunchPadSkill extends Skill {
+
 	@getter ("buttonPressed")
 	public String getButtonPressed() {
 		String name;
-		if(LaunchPadEventLayer.pressedButton!=null){
-			name =  LaunchPadEventLayer.pressedButton.name();	
-		}else{
-			name="NULL";
+		if (LaunchPadEventLayer.pressedButton != null) {
+			name = LaunchPadEventLayer.pressedButton.name();
+		} else {
+			name = "NULL";
 		}
 		return name;
 	}
 
-	/*@setter ("buttonPressed" )
-	public void setButtonPressed(String s) {
-		if(LaunchPadEventLayer.pressedButton!=null){
-		  LaunchPadEventLayer.client.setButtonLight(LaunchPadEventLayer.pressedButton, Color.RED, BackBufferOperation.NONE);
-		}
-	}*/
-	
+	/*
+	 * @setter ("buttonPressed" ) public void setButtonPressed(String s) { if(LaunchPadEventLayer.pressedButton!=null){
+	 * LaunchPadEventLayer.client.setButtonLight(LaunchPadEventLayer.pressedButton, Color.RED,
+	 * BackBufferOperation.NONE); } }
+	 */
+
 	@getter ("padPressed")
 	public ILocation getPadPressed() {
-		GamaPoint p = new GamaPoint(LaunchPadEventLayer.pressedPad.getX(),LaunchPadEventLayer.pressedPad.getY());
-		return p;	
+		final GamaPoint p = new GamaPoint(LaunchPadEventLayer.pressedPad.getX(), LaunchPadEventLayer.pressedPad.getY());
+		return p;
 	}
 
-	/*@setter ("padPressed" )
-	public void setPadPressed(ILocation l) {
-		if(LaunchPadEventLayer.pressedPad!=null){
-			LaunchPadEventLayer.client.setPadLight(LaunchPadEventLayer.pressedPad, Color.RED, BackBufferOperation.NONE);
-		}
-	}*/
-	
-	
-	
+	/*
+	 * @setter ("padPressed" ) public void setPadPressed(ILocation l) { if(LaunchPadEventLayer.pressedPad!=null){
+	 * LaunchPadEventLayer.client.setPadLight(LaunchPadEventLayer.pressedPad, Color.RED, BackBufferOperation.NONE); } }
+	 */
+
 	@action (
 			name = "resetPad",
 			doc = @doc (
@@ -90,8 +71,7 @@ public class LaunchPadSkill  extends Skill{
 		LaunchPadEventLayer.client.reset();
 		return;
 	}
-	
-	
+
 	@action (
 			name = "setPadLight",
 			args = { @arg (
@@ -108,24 +88,30 @@ public class LaunchPadSkill  extends Skill{
 	public void setPadLight(final IScope scope) throws GamaRuntimeException {
 		final String colorString = scope.hasArg("color") ? (String) scope.getArg("color", IType.STRING) : "black";
 		System.out.println("colorString" + colorString);
-		if(LaunchPadEventLayer.pressedPad!=null){	
-			if(colorString.equals("amber")){
-				LaunchPadEventLayer.client.setPadLight(LaunchPadEventLayer.pressedPad, Color.AMBER, BackBufferOperation.NONE);	
+		if (LaunchPadEventLayer.pressedPad != null) {
+			if (colorString.equals("amber")) {
+				LaunchPadEventLayer.client.setPadLight(LaunchPadEventLayer.pressedPad, Color.AMBER,
+						BackBufferOperation.NONE);
 			}
-			if(colorString.equals("black")){
-				LaunchPadEventLayer.client.setPadLight(LaunchPadEventLayer.pressedPad, Color.BLACK, BackBufferOperation.NONE);	
+			if (colorString.equals("black")) {
+				LaunchPadEventLayer.client.setPadLight(LaunchPadEventLayer.pressedPad, Color.BLACK,
+						BackBufferOperation.NONE);
 			}
-			if(colorString.equals("green")){
-				LaunchPadEventLayer.client.setPadLight(LaunchPadEventLayer.pressedPad, Color.GREEN, BackBufferOperation.NONE);	
+			if (colorString.equals("green")) {
+				LaunchPadEventLayer.client.setPadLight(LaunchPadEventLayer.pressedPad, Color.GREEN,
+						BackBufferOperation.NONE);
 			}
-			if(colorString.equals("orange")){
-				LaunchPadEventLayer.client.setPadLight(LaunchPadEventLayer.pressedPad, Color.ORANGE, BackBufferOperation.NONE);	
+			if (colorString.equals("orange")) {
+				LaunchPadEventLayer.client.setPadLight(LaunchPadEventLayer.pressedPad, Color.ORANGE,
+						BackBufferOperation.NONE);
 			}
-			if(colorString.equals("red")){
-				LaunchPadEventLayer.client.setPadLight(LaunchPadEventLayer.pressedPad, Color.RED, BackBufferOperation.NONE);	
+			if (colorString.equals("red")) {
+				LaunchPadEventLayer.client.setPadLight(LaunchPadEventLayer.pressedPad, Color.RED,
+						BackBufferOperation.NONE);
 			}
-			if(colorString.equals("yellow")){
-				LaunchPadEventLayer.client.setPadLight(LaunchPadEventLayer.pressedPad, Color.YELLOW, BackBufferOperation.NONE);	
+			if (colorString.equals("yellow")) {
+				LaunchPadEventLayer.client.setPadLight(LaunchPadEventLayer.pressedPad, Color.YELLOW,
+						BackBufferOperation.NONE);
 			}
 		}
 		return;
