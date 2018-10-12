1d82a3d211373b73bfbcc8d95db71d6a328a6cd4
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2018-10-13 00:19:33 +0700

    gamr zip menu

--- ummisco.gama.gamr/src/jobs/JobDocumentationProject.java
+++ ummisco.gama.gamr/src/jobs/JobDocumentationProject.java
@@ -1,71 +1,102 @@
 package jobs;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 
-import markdownSyntactic.IParser;
+import msi.gama.precompiler.GamlAnnotations.file;
+import msi.gama.runtime.GAMA;
 import ummisco.gama.ui.navigator.contents.WrappedResource;
+import ummisco.gama.ui.utils.SwtGui;
+
 /**
  * 
- * @author damienphilippon
- * Date : 19 Dec 2017
- * Class used to do a Documentation job for a Project (will defined an Index of all the species and experiments of the
- * projects, and do the documentation of all the models contained in the project)
+ * @author damienphilippon Date : 19 Dec 2017 Class used to do a Documentation
+ *         job for a Project (will defined an Index of all the species and
+ *         experiments of the projects, and do the documentation of all the
+ *         models contained in the project)
  */
-public class JobDocumentationProject extends JobDocumentation{
-	
+public class JobDocumentationProject extends JobDocumentation {
+
 	/**
 	 * The project concerned by this job
 	 */
 	public IProject project;
 
 	/**
-	 * Constructor using a given project (will be saved in a default documentation folder )
-	 * @param project {@code IProject} the project to document
+	 * Constructor using a given project (will be saved in a default documentation
+	 * folder )
+	 * 
+	 * @param project
+	 *            {@code IProject} the project to document
 	 */
-	public JobDocumentationProject(IProject project)
-	{
-		super(project.getLocation().toOSString()+File.pathSeparator+IParser.PROJECT_DEFAULT_FOLDER);
-		this.project= project;
+	public JobDocumentationProject(IProject project) {
+		super(project.getLocation().toOSString());
+		this.project = project;
 	}
+
 	/**
-	 * Constructor using a given project (will be saved in a given documentation folder )
-	 * @param project {@code IProject} the project to document
-	 * @param path {@code String} the path (String) in which the documentation will be saved
-	 */public JobDocumentationProject(IProject project, String path)
-	{
+	 * Constructor using a given project (will be saved in a given documentation
+	 * folder )
+	 * 
+	 * @param project
+	 *            {@code IProject} the project to document
+	 * @param path
+	 *            {@code String} the path (String) in which the documentation will
+	 *            be saved
+	 */
+	public JobDocumentationProject(IProject project, String path) {
 		super(path);
-		this.project= project;
+		this.project = project;
 	}
-	
-	
-	
-	 /**
+
+	public void pack(String sourceDirPath, String zipFilePath) throws IOException {
+		System.out.println(System.getProperty("user.dir"));
+		Files.deleteIfExists(Paths.get(zipFilePath));
+		Path p = Files.createFile(Paths.get(zipFilePath));
+		try (ZipOutputStream zs = new ZipOutputStream(Files.newOutputStream(p))) {
+			Path pp = Paths.get(sourceDirPath);
+			Files.walk(pp).filter(path -> !Files.isDirectory(path)).forEach(path -> {
+				ZipEntry zipEntry = new ZipEntry(pp.relativize(path).toString());
+				try {
+					zs.putNextEntry(zipEntry);
+					Files.copy(path, zs);
+					zs.closeEntry();
+				} catch (IOException e) {
+					System.err.println(e);
+				}
+			});
+		}
+	}
+
+	/**
 	 * Method to run the job in background
 	 */
 	public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException {
-		if(project!=null)
-		{
-			WrappedResource file_to_convert = ummisco.gama.ui.navigator.contents.ResourceManager.getInstance().findWrappedInstanceOf(project);
-			Object[] childrenResources = file_to_convert.getNavigatorChildren().clone();
-			for(Object aResource : childrenResources)
-			{
-				if(aResource instanceof WrappedResource)
-				{
-					if(((WrappedResource) aResource).countModels()>0)
-					{
-						generateForWrappedResource((WrappedResource) aResource);
-					}
-				}
+		if (project != null) {
+			// WrappedResource file_to_convert =
+			// ummisco.gama.ui.navigator.contents.ResourceManager.getInstance().findWrappedInstanceOf(project);
+			// Object[] childrenResources = file_to_convert.getNavigatorChildren().clone();
+			try {
+				pack(project.getLocation().toOSString(), directory + project.getName() + ".zip");
+				GAMA.getGui().tell("Zip created!");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			childrenResources=null;
-			generateIndex();
 		}
-		this.dispose();
+		// this.dispose();
 		return Status.OK_STATUS;
 	}
 }
--- ummisco.gama.gamr/src/markdownSyntactic/GenerateMDHandler.java
+++ ummisco.gama.gamr/src/markdownSyntactic/GenerateMDHandler.java
@@ -23,8 +23,7 @@ import org.eclipse.ui.dialogs.FilteredResourcesSelectionDialog;
 import org.eclipse.ui.dialogs.ListDialog;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-import jobs.JobDocumentation;
-import jobs.JobDocumentationModelIndependent;
+import jobs.JobDocumentation; 
 import jobs.JobDocumentationProject;
 import msi.gama.runtime.GAMA;
 import msi.gama.util.file.GamlFileInfo;
@@ -50,48 +49,8 @@ public class GenerateMDHandler extends AbstractHandler {
 	 * Method that will execute the job (will ask for a location)
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		Shell shell = HandlerUtil.getActiveWorkbenchWindow(event).getShell();
-		final IContainer p = ResourcesPlugin.getWorkspace().getRoot();
-		dialog = new FilteredResourcesSelectionDialog(shell, false, p, Resource.FILE);
-		dialog.setInitialPattern("*.gaml");
-		dialog.setTitle("Choose a gaml model in project " + p.getName());
-		if (dialog.open() == Window.OK) {
-			final Object[] result = dialog.getResult();
-			if (result.length == 1) {
-				final IResource res = (IResource) result[0];
-				System.out.println(res.getFullPath().toString());
-
-				final IGamaFileMetaData data = GAMA.getGui().getMetaDataProvider().getMetaData(res, false, true);
-				if (data != null && data instanceof GamlFileInfo) {
-					String[] exp = (String[]) ((GamlFileInfo) data).getExperiments().toArray();
-					LabelProvider ss = new LabelProvider();
-					ElementListSelectionDialog l = new ElementListSelectionDialog(shell, ss);
-					l.setMessage("Selection the experiment to be launch in GAMR");
-					l.setElements(((GamlFileInfo) data).getExperiments().toArray());
-					int rres = l.open();
-					if (rres == Window.OK) {
-						Object[] objs = l.getResult();
-						System.out.println(objs[0]);
-					}
-					return null;
-					// String input = (String) JOptionPane.showInputDialog(null, "Choose now...",
-					// "The Choice of a Lifetime", JOptionPane.QUESTION_MESSAGE, null, // Use
-					// // default
-					// // icon
-					// exp, // Array of choices
-					// exp[0]); // Initial choice
-					// System.out.println(input);
-				}
-
-				// modelChooser.setText(res.getFullPath().toString());
-			}
-		}
 		ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().getSelection();
 
-		// Define the output directory
-		DirectoryDialog directorySelector = new DirectoryDialog(Display.getCurrent().getActiveShell());
-		directorySelector.setMessage("Select a directory as a location for documentation");
-		directorySelector.setText("Directory for Documentation");
 
 		if (selection != null & selection instanceof IStructuredSelection) {
 
@@ -100,8 +59,38 @@ public class GenerateMDHandler extends AbstractHandler {
 			if (o instanceof WrappedResource) {
 				WrappedResource file_to_convert = (WrappedResource) o;
 
+				Shell shell = HandlerUtil.getActiveWorkbenchWindow(event).getShell();
+				final IContainer p = ResourcesPlugin.getWorkspace().getRoot();
+				dialog = new FilteredResourcesSelectionDialog(shell, false, p, Resource.FILE);
+				dialog.setInitialPattern(file_to_convert.getProject().getName()+"/*.gaml");
+				dialog.setTitle("Choose a gaml model in project " + file_to_convert.getProject().getName());
+				if (dialog.open() == Window.OK) {
+					final Object[] result = dialog.getResult();
+					if (result.length == 1) {
+						final IResource res = (IResource) result[0];
+						System.out.println(res.getFullPath().toString());
+
+						final IGamaFileMetaData data = GAMA.getGui().getMetaDataProvider().getMetaData(res, false, true);
+						if (data != null && data instanceof GamlFileInfo) {
+							LabelProvider ss = new LabelProvider();
+							ElementListSelectionDialog l = new ElementListSelectionDialog(shell, ss);
+							l.setMessage("Selection the experiment to be launch in GAMR");
+							l.setElements(((GamlFileInfo) data).getExperiments().toArray());
+							int rres = l.open();
+							if (rres == Window.OK) {
+								Object[] objs = l.getResult();
+								System.out.println(objs[0]);
+							} 
+						}
+
+					}
+				}
 				// If the selected object is a project, then creates a Job for project
 				if (o instanceof WrappedProject) {
+					// Define the output directory
+					DirectoryDialog directorySelector = new DirectoryDialog(Display.getCurrent().getActiveShell());
+					directorySelector.setMessage("Select a directory as a location for documentation");
+					directorySelector.setText("Directory for Documentation");
 					// Do for a project
 					directorySelector
 							.setFilterPath(file_to_convert.getProject().getResource().getLocation().toOSString());
