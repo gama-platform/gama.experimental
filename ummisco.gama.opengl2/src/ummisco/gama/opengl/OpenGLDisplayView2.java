5b60ae6e4b1fdcbae509e563f60daaae051620f7
Huynh Quang-Nghi <hqnghi88@gmail.com>
2018-12-06 12:28:09 +0700

    opengl2

---
+++ ummisco.gama.opengl2/src/ummisco/gama/opengl/OpenGLDisplayView2.java
@@ -0,0 +1,65 @@
+/*********************************************************************************************
+ *
+ * 'OpenGLDisplayView.java, in plugin ummisco.gama.opengl, is part of the source code of the GAMA modeling and
+ * simulation platform. (c) 2007-2016 UMI 209 UMMISCO IRD/UPMC & Partners
+ *
+ * Visit https://github.com/gama-platform/gama for license information and developers contact.
+ * 
+ *
+ **********************************************************************************************/
+package ummisco.gama.opengl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.swt.widgets.Composite;
+
+import ummisco.gama.ui.views.displays.SWTDisplayView;
+
+/**
+ * Class OpenGLLayeredDisplayView.
+ *
+ * @author drogoul
+ * @since 25 mars 2015
+ *
+ */
+public class OpenGLDisplayView2 extends SWTDisplayView {
+
+	public static String ID = "msi.gama.application.view.OpenGLDisplayView2";
+
+	@Override
+	public SWTOpenGLDisplaySurface getDisplaySurface() {
+		return (SWTOpenGLDisplaySurface) super.getDisplaySurface();
+	}
+
+	@Override
+	protected Composite createSurfaceComposite(final Composite parent) {
+		final SWTOpenGLDisplaySurface surface = new SWTOpenGLDisplaySurface(parent, getOutput());
+		surfaceComposite = surface.renderer.getCanvas();
+		surface.outputReloaded();
+		return surfaceComposite;
+	}
+
+	@Override
+	public boolean forceOverlayVisibility() {
+		final SWTOpenGLDisplaySurface surface = getDisplaySurface();
+		return surface != null && surface.getROIDimensions() != null;
+	}
+
+	@Override
+	public List<String> getCameraNames() {
+		return new ArrayList<String>(getDisplaySurface().renderer.camera.PRESETS.keySet());
+	}
+
+	@Override
+	public void hideToolbar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showToolbar() {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ ummisco.gama.opengl2/src/ummisco/gama/opengl/scene/ModelScene.java
@@ -0,0 +1,264 @@
+/*********************************************************************************************
+ *
+ * 'ModelScene.java, in plugin ummisco.gama.opengl, is part of the source code of the GAMA modeling and simulation
+ * platform. (c) 2007-2016 UMI 209 UMMISCO IRD/UPMC & Partners
+ *
+ * Visit https://github.com/gama-platform/gama for license information and developers contact.
+ * 
+ *
+ **********************************************************************************************/
+package ummisco.gama.opengl.scene;
+
+import java.awt.image.BufferedImage;
+import java.util.Map;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+import msi.gama.common.interfaces.ILayer;
+import msi.gama.metamodel.shape.GamaPoint;
+import msi.gama.util.TOrderedHashMap;
+import msi.gama.util.file.GamaGeometryFile;
+import msi.gama.util.file.GamaImageFile;
+import msi.gaml.statements.draw.DrawingAttributes;
+import msi.gaml.statements.draw.FieldDrawingAttributes;
+import msi.gaml.statements.draw.FileDrawingAttributes;
+import msi.gaml.statements.draw.ShapeDrawingAttributes;
+import ummisco.gama.opengl.Abstract3DRenderer;
+
+/**
+ *
+ * The class ModelScene. A repository for all the objects that constitute the scene of a model : strings, images,
+ * shapes... 04/03/14: Now organized by layers to address the issue of z depth
+ *
+ * @author drogoul
+ * @since 3 mai 2013
+ *
+ */
+public class ModelScene {
+
+	public static final String AXES_KEY = "__axes__0";
+	public static final String FRAME_KEY = "__frame__0";
+	public static final String ROTATION_HELPER_KEY = "__rotation__0";
+	public static final String KEYSTONE_HELPER_KEY = "__keystone__0";
+	public static final String LIGHTS_KEY = "__lights__0";
+	public static final String FPS_KEY = "z__fps__0";
+	protected final TOrderedHashMap<String, LayerObject> layers = new TOrderedHashMap<String, LayerObject>();
+	protected LayerObject currentLayer;
+	protected final Abstract3DRenderer renderer;
+	private volatile boolean rendered = false;
+	private volatile int objectNumber;
+	private double zIncrement;
+	private int currentLayerTrace;
+
+	public static abstract class ObjectVisitor {
+		public abstract void process(AbstractObject object);
+	}
+
+	public ModelScene(final Abstract3DRenderer renderer, final boolean withWorld) {
+		this.renderer = renderer;
+		if (withWorld) {
+			initWorld();
+		}
+	}
+
+	protected void initWorld() {
+//		if (renderer.data.isDrawEnv()) {
+//			layers.put(FRAME_KEY, new FrameLayerObject(renderer));
+//			layers.put(AXES_KEY, new AxesLayerObject(renderer));
+//		}
+//		if (renderer.useShader()) {
+//			layers.put(ROTATION_HELPER_KEY, new RotationHelperLayerObject(renderer));
+////			layers.put(KEYSTONE_HELPER_KEY, new KeystoneHelperLayerObject(renderer));
+//			layers.put(LIGHTS_KEY, new LightsLayerObject(renderer));
+//			if (renderer.data.isShowfps())
+//				layers.put(FPS_KEY, new FPSLayerObject(renderer));
+//		}
+	}
+
+	/**
+	 * @param context
+	 *            Called every new iteration when updateDisplay() is called on the surface
+	 */
+	public void wipe(final OpenGL gl) {
+
+		for (final Map.Entry<String, LayerObject> entry : layers.entrySet()) {
+			final LayerObject obj = entry.getValue();
+			if (obj != null && (!obj.isStatic() || obj.isInvalid())) {
+				obj.clear(gl);
+			}
+		}
+		// Wipe the textures.
+		gl.deleteVolatileTextures();
+	}
+
+	public void draw(final OpenGL gl) {
+
+//		if (renderer.useShader()) {
+//			// if the rotation helper layer exists, put it at the end of the map
+//			// (otherwise, transparency issues)
+//			final LayerObject rotLayer = layers.get(ROTATION_HELPER_KEY);
+//			if (rotLayer != null) {
+//				layers.remove(ROTATION_HELPER_KEY);
+//				layers.put(ROTATION_HELPER_KEY, rotLayer);
+//			}
+//		}
+		gl.pushMatrix();
+		gl.setZIncrement(zIncrement);
+
+		for (final LayerObject layer : layers.values()) {
+			if (layer != null && !layer.isInvalid()) {
+				try {
+					layer.draw(gl);
+					layer.lock();
+				} catch (final RuntimeException r) {
+					System.err.println("Runtime error " + r.getMessage() + " in OpenGL loop");
+					r.printStackTrace();
+				}
+			}
+		}
+		gl.setZIncrement(0);
+		rendered = true;
+		gl.popMatrix();
+	}
+
+	private double computeVisualZIncrement() {
+		if (objectNumber == 0)
+			return 0d;
+		// The maximum visual z allowance between the object at the bottom and the one at the top
+		final double maxZ = renderer.getMaxEnvDim() / 2000d;
+		// The increment is simply
+		return maxZ / objectNumber;
+	}
+
+	public boolean cannotAdd() {
+		if (currentLayer == null)
+			return true;
+		return currentLayer.isStatic() && currentLayer.isLocked();
+	}
+
+	private <T extends AbstractObject> T configure(final T object) {
+		objectNumber += currentLayerTrace;
+		return object;
+	}
+
+	public StringObject addString(final String string, final DrawingAttributes attributes) {
+		if (cannotAdd()) { return null; }
+		return configure(currentLayer.addString(string, attributes));
+	}
+
+	public GeometryObject addImageFile(final GamaImageFile file, final FileDrawingAttributes attributes) {
+		if (cannotAdd()) { return null; }
+		return configure(currentLayer.addImage(file, attributes));
+	}
+
+	public ResourceObject addGeometryFile(final GamaGeometryFile file, final FileDrawingAttributes attributes) {
+		if (cannotAdd()) { return null; }
+		return configure(currentLayer.addFile(file, attributes));
+	}
+
+	public GeometryObject addImage(final BufferedImage img, final DrawingAttributes attributes) {
+		if (cannotAdd()) { return null; }
+		return configure(currentLayer.addImage(img, attributes));
+	}
+
+	public GeometryObject addGeometry(final Geometry geometry, final ShapeDrawingAttributes attributes) {
+		if (cannotAdd()) { return null; }
+		return configure(currentLayer.addGeometry(geometry, attributes));
+	}
+
+	public FieldObject addField(final double[] fieldValues, final FieldDrawingAttributes attributes) {
+		if (cannotAdd()) { return null; }
+		return configure(currentLayer.addField(fieldValues, attributes));
+	}
+
+	public void dispose() {
+		layers.clear();
+		currentLayer = null;
+	}
+
+	public void beginDrawingLayers() {
+		currentLayerTrace = 0;
+	}
+
+	public void endDrawingLayers() {
+		zIncrement = computeVisualZIncrement();
+	}
+
+	public boolean rendered() {
+		return rendered;
+	}
+
+	public void reload() {
+		for (final LayerObject l : layers.values()) {
+			l.unlock();
+		}
+		dispose();
+		initWorld();
+	}
+
+	public void hideLayer(final String name) {
+		layers.put(name, null);
+	}
+
+	public void beginDrawingLayer(final ILayer layer, final GamaPoint offset, final GamaPoint scale,
+			final Double alpha) {
+		final int id = layer.getDefinition().getOrder();
+		final String key = layer.getName() + id;
+		currentLayer = layers.get(key);
+		if (currentLayer == null) {
+			currentLayer = new LayerObject(renderer, layer);
+			layers.put(key, currentLayer);
+		}
+		currentLayer.setOffset(offset);
+		currentLayer.setScale(scale);
+		currentLayer.setAlpha(alpha);
+		currentLayerTrace = currentLayer.numberOfTraces();
+	}
+
+	public void beginOverlay() {
+		// currentLayer.setOverlay(true);
+	}
+
+	/**
+	 * @return
+	 */
+	public ModelScene copyStatic() {
+		final ModelScene newScene = new ModelScene(renderer, false);
+		for (final Map.Entry<String, LayerObject> entry : layers.entrySet()) {
+			final LayerObject layer = entry.getValue();
+			if ((layer.isStatic() || layer.hasTrace()) && !layer.isInvalid()) {
+				newScene.layers.put(entry.getKey(), layer);
+			}
+		}
+		return newScene;
+	}
+
+	/**
+	 *
+	 */
+	public void invalidateLayers() {
+		for (final Map.Entry<String, LayerObject> entry : layers.entrySet()) {
+			entry.getValue().invalidate();
+		}
+	}
+
+	public void startDrawRotationHelper(final GamaPoint pivotPoint, final double size) {
+		final AxesLayerObject worldLayer = (AxesLayerObject) layers.get(AXES_KEY);
+		if (worldLayer != null) {
+			worldLayer.setOffset(pivotPoint.yNegated());
+			final double ratio = size / renderer.getMaxEnvDim();
+			worldLayer.setScale(new GamaPoint(ratio, ratio, ratio));
+
+		}
+	}
+
+	public void stopDrawRotationHelper() {
+		final AxesLayerObject worldLayer = (AxesLayerObject) layers.get(AXES_KEY);
+		if (worldLayer != null) {
+			worldLayer.setOffset(null);
+			worldLayer.setScale(null);
+		}
+
+	}
+
+}
