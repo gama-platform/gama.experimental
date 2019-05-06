1f299edec888d21433078ad092957e1fd9f9494d
HUYNH Quang Nghi <hqnghi88@gmail.com>
2019-05-06 09:17:33 +0700

    fix gamr generator, wrong path

--- ummisco.gama.gamr/src/jobs/JobDocumentationProject.java
+++ ummisco.gama.gamr/src/jobs/JobDocumentationProject.java
@@ -10,28 +10,26 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.ui.actions.WorkspaceModifyOperation;
 
 import msi.gama.precompiler.GamlAnnotations.file;
 import msi.gama.runtime.GAMA;
 import ummisco.gama.ui.navigator.contents.WrappedResource;
 import ummisco.gama.ui.utils.SwtGui;
-
-/**
- * 
- * @author damienphilippon Date : 19 Dec 2017 Class used to do a Documentation
- *         job for a Project (will defined an Index of all the species and
- *         experiments of the projects, and do the documentation of all the
- *         models contained in the project)
- */
+ 
 public class JobDocumentationProject extends JobDocumentation {
 
 	/**
@@ -87,7 +85,6 @@ public class JobDocumentationProject extends JobDocumentation {
 			});
 		}
 	}
-	
 	public void saveIni(String dirPath) {
 		new File(dirPath+"/.metadata").mkdir();
 
@@ -119,12 +116,13 @@ public class JobDocumentationProject extends JobDocumentation {
 	 */
 	public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException {
 		if (project != null) {
+
 			// WrappedResource file_to_convert =
 			// ummisco.gama.ui.navigator.contents.ResourceManager.getInstance().findWrappedInstanceOf(project);
 			// Object[] childrenResources = file_to_convert.getNavigatorChildren().clone();
 			try {
 				saveIni(project.getLocation().toOSString());
-				pack(project.getLocation().toOSString(), directory + project.getName() + ".gamr");
+				pack(project.getLocation().toOSString(), directory+"/" + project.getName() + ".gamr");
 				GAMA.getGui().tell("Gamr created!");
 				new File(project.getLocation().toOSString()+"/.metadata/launcher.ini").delete();
 				new File(project.getLocation().toOSString()+"/.metadata").delete();
