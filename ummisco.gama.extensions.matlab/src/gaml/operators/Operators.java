30e87a63492a538322bfc18cedf1167b3d1ebeca
benoitgaudou <benoit.gaudou@gmail.com>
2019-06-16 04:09:03 +0200

    PLugin and feature matlab: 1st working version

--- ummisco.gama.extensions.matlab/src/gaml/operators/Operators.java
+++ ummisco.gama.extensions.matlab/src/gaml/operators/Operators.java
@@ -1,12 +1,7 @@
 package gaml.operators;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 
-import com.mathworks.engine.EngineException;
 import com.mathworks.engine.MatlabEngine;
 
 import gaml.types.GamaMatlabEngine;
@@ -15,15 +10,16 @@ import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
-import msi.gama.precompiler.GamlAnnotations.doc;
-import msi.gama.precompiler.GamlAnnotations.example;
+import msi.gama.precompiler.ITypeProvider;
+import msi.gama.precompiler.Reason;
 import msi.gama.precompiler.GamlAnnotations.no_test;
 import msi.gama.precompiler.GamlAnnotations.operator;
 import msi.gama.runtime.IScope;
 import msi.gama.runtime.exceptions.GamaRuntimeException;
-import msi.gama.util.GamaList;
+import msi.gama.util.GamaListFactory;
 import msi.gama.util.IList;
-import msi.gaml.types.GamaListType;
+import msi.gaml.expressions.IExpression;
+import msi.gaml.types.GamaMatrixType;
 import msi.gaml.types.IType;
 import msi.gaml.types.Types;
 
@@ -34,60 +30,9 @@ public class Operators {
 //			@example(value = "as_driving_graph(road,node)  --:  build a graph while using the road agents as edges and the node agents as nodes", isExecutable = false) }, 
 //	see = {"as_intersection_graph", "as_distance_graph", "as_edge_graph" })
 //	@no_test
-	public static List<Double> test(final IScope scope, final String n) {
-		try {
-	        //Start MATLAB asynchronously
-	        Future<MatlabEngine> eng = MatlabEngine.startMatlabAsync();
-	
-	     // Get engine instance from the future result
-	        MatlabEngine ml = eng.get();
-	
-	    /*
-	     * Find elements greater than 5
-	     * 1. Create input matrix
-	     * 2. Put variable matrix in the MATLAB base workspace
-	     * 3. Solve A(A>5) in MATLAB
-	     * 4. Return results and display
-	     * 5. Call the power function in MATLAB on the returned matrix
-	     * 6. Display results
-	     */
-	        double[][] input = new double[4][4];
-	        for (int i = 0; i < 4; i++) {
-	            for (int j = 0; j < 4; j++) {
-	                double num = Math.random() * 10;
-	                input[i][j] = num;
-	            }
-	        }
-	        System.out.println("\nFind numbers from a matrix that are greater than five and square them:\n");
-	        System.out.println("Input matrix: ");
-	        for (int i = 0; i < 4; i++) {
-	            {
-	                for (int j = 0; j < 4; j++) {
-	                    System.out.print(String.format("%.2f", input[i][j]) + "\t");
-	                }
-	                System.out.print("\n");
-	            }
-	        }
-	
-	        // Put the matrix in the MATLAB workspace
-	        ml.putVariableAsync("A", input);
-	
-	        // Evaluate the command to search in MATLAB
-	        ml.eval("B=A(A>5);");
-	
-	        // Get result from the workspace
-	        Future<double[]> futureEval = ml.getVariableAsync("B");
-	        double[] output = futureEval.get();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-        
-        List<Double> l = new ArrayList<Double>();
-        return l;
-		
-	}
 
 	@operator(value = "test_matlab")
+	@no_test(Reason.IMPOSSIBLE_TO_TEST)
 	public static boolean testConnection(final IScope scope, final boolean isAsync) {
         //Start MATLAB asynchronously
 		try {
@@ -110,7 +55,7 @@ public class Operators {
 	        // Get result from the workspace
 	        Future<double[]> futureEval = eng.getVariableAsync("B");
 	        double[] output = futureEval.get();		
-			
+	        
 	        eng.disconnect();			
 		} catch ( IllegalArgumentException | IllegalStateException | InterruptedException | ExecutionException e) {
 		    throw GamaRuntimeException.error("Connection to Matlab Failed", scope);
@@ -119,26 +64,93 @@ public class Operators {
 	}
 
 	@operator(value = "eval")
-	public static IList eval(final IScope scope, final GamaMatlabEngine gamaEng, final String exp, final String variableName) {
-		 double[] output;		
-        // Evaluate the command to search in MATLAB
-		try {
-			gamaEng.getEngine().eval(exp);
-	        // Get result from the workspace
-		//	Object eval = gamaEng.getEngine().get 
-			
-			
-	        Future<double[]> futureEval = gamaEng.getEngine().getVariableAsync(variableName);
-	        output = futureEval.get();			
+	@no_test(Reason.IMPOSSIBLE_TO_TEST)	
+	public static Object evalOp(final IScope scope, final GamaMatlabEngine gamaEng, final String exp, final String variableName) {
+		return eval(scope, gamaEng, exp, variableName);
+	}
+	
+	@operator(value = "eval", type = ITypeProvider.TYPE_AT_INDEX + 3)
+	@no_test(Reason.IMPOSSIBLE_TO_TEST)	
+	public static Object evalOp(final IScope scope, final GamaMatlabEngine gamaEng, final String exp, final IExpression variable) {
+		return eval(scope, gamaEng, exp, variable.getName());
+	}
+
+	@operator(value = "eval", type = ITypeProvider.TYPE_AT_INDEX + 4)
+	@no_test(Reason.IMPOSSIBLE_TO_TEST)	
+	public static Object evalOp(final IScope scope, final GamaMatlabEngine gamaEng, final String exp, final String variableName, final IType type) {
+		return eval(scope, gamaEng, exp, variableName);
+	}
+
+	public static Object eval(final IScope scope, final GamaMatlabEngine gamaEng, final String exp, final String variableName) {
+		 try {
+			gamaEng.getEngine().eval(exp);			
 		} catch (CancellationException | InterruptedException | ExecutionException e) {
 		    throw GamaRuntimeException.error("MATLAB evaluation failed", scope);
 		}
 
-		return GamaListType.staticCast(scope, output, Types.FLOAT, true);
+		return getVariable(scope, gamaEng, variableName);			
+	}	
+	
+	@operator(value = "get_variable")
+	@no_test(Reason.IMPOSSIBLE_TO_TEST)
+	public static Object getVariableOp(final IScope scope, final GamaMatlabEngine gamaEng, final String variableName) {
+		return getVariable(scope,gamaEng,variableName);
+	}	
+	
+	@operator(value = "get_variable", type = ITypeProvider.TYPE_AT_INDEX + 3)
+	@no_test(Reason.IMPOSSIBLE_TO_TEST)
+	public static Object getVariableOp(final IScope scope, final GamaMatlabEngine gamaEng, final IExpression variable) {
+		return getVariable(scope,gamaEng,variable.getName());
+	}	
+	
+	@operator(value = "get_variable", type = ITypeProvider.TYPE_AT_INDEX + 4)
+	@no_test(Reason.IMPOSSIBLE_TO_TEST)
+	public static Object getVariableOp(final IScope scope, final GamaMatlabEngine gamaEng, final String exp, final String variableName, final IType type) {
+		return getVariable(scope,gamaEng,variableName);
+	}	
+	
+	public static Object getVariable(final IScope scope, final GamaMatlabEngine gamaEng,  final String variableName) {
+		 Object output;
+
+		 try {
+			output = gamaEng.getEngine().getVariable(variableName);			
+		 } catch (CancellationException | InterruptedException | ExecutionException e) {
+		    throw GamaRuntimeException.error("MATLAB evaluation failed", scope);
+		 }
+		 
+		 output = Matlab2GamaType(scope, output);
+
+		return output;		
 	}
+
 	
+	private static Object Matlab2GamaType(IScope scope, Object output) {
+
+		if(output instanceof double[]) {
+			return GamaListFactory.create(scope, Types.FLOAT, (double[]) output ) ;
+		} else if(output instanceof double[][]) {
+			// Complicated because we need to transpose the matrix provided by matlab 
+			double[][] mat = (double[][]) output;
+			if(mat.length == 0) {return 0.0;}
+			
+			IList<IList<?>> lmat = GamaListFactory.create(Types.LIST);
 	
+			for(int i = 0;i < mat[0].length ; i++) {
+				ArrayList<Double> l = new ArrayList<>();
+				for(int j = 0 ; j < mat.length ; j ++ ) {
+					l.add(mat[j][i]);
+				}
+				lmat.add(GamaListFactory.create(scope, Types.FLOAT, l));
+			}
+			return GamaMatrixType.staticCast(scope, lmat, null, Types.FLOAT, true);
+		}
+		 
+		// Other cases checked : float and bool
+		return output;
+	}
+
 	@operator(value = "get_matlab_engine")
+	@no_test(Reason.IMPOSSIBLE_TO_TEST)	
 	public static GamaMatlabEngine getGamaEngine(final IScope scope, final boolean isAsync) {
 		MatlabEngine ml;
 		try {	
@@ -159,76 +171,10 @@ public class Operators {
 	}
 
 	@operator(value = "close_matlab_engine")
+	@no_test(Reason.IMPOSSIBLE_TO_TEST)	
 	public static Boolean closeGamaEngine(final IScope scope, final GamaMatlabEngine gamaEng, final boolean aSync) {
 		gamaEng.disconnect(scope,aSync);
 		return true;
 	}
 
-	
-     // Get engine instance from the future result
-     //   MatlabEngine ml = eng.
-  
-/*        
-         * Find elements greater than 5
-         * 1. Create input matrix
-         * 2. Put variable matrix in the MATLAB base workspace
-         * 3. Solve A(A>5) in MATLAB
-         * 4. Return results and display
-         * 5. Call the power function in MATLAB on the returned matrix
-         * 6. Display results
-         * /
-            double[][] input = new double[4][4];
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
-                    double num = Math.random() * 10;
-                    input[i][j] = num;
-                }
-            }
-            System.out.println("\nFind numbers from a matrix that are greater than five and square them:\n");
-            System.out.println("Input matrix: ");
-            for (int i = 0; i < 4; i++) {
-                {
-                    for (int j = 0; j < 4; j++) {
-                        System.out.print(String.format("%.2f", input[i][j]) + "\t");
-                    }
-                    System.out.print("\n");
-                }
-            }
-
-            // Put the matrix in the MATLAB workspace
-            ml.putVariableAsync("A", input);
-
-            // Evaluate the command to search in MATLAB
-            ml.eval("B=A(A>5);");
-
-            // Get result from the workspace
-            Future<double[]> futureEval = ml.getVariableAsync("B");
-            double[] output = futureEval.get();
-
-            // Display result
-            System.out.println("\nElements greater than 5: ");
-            for (int i = 0; i < output.length; i++) {
-                System.out.print(" " + String.format("%.2f", output[i]));
-            }
-
-            // Square the returned elements using the power function in MATLAB
-            double[] powResult = ml.feval("power", output, Double.valueOf(2));
-            System.out.println("\n\nSquare of numbers greater than 5:");
-            for (int i = 0; i < powResult.length; i++) {
-
-                //Set precision for the output values
-                System.out.print(" " + String.format("%.2f", powResult[i]));
-            }
-            System.out.println("\n");        
-   */     
-
-	
-
-
-    public static void main(String args[]) {
-    	
-    	
-       	List l = Operators.test(null, "hello");
-    }	
-	
 }
