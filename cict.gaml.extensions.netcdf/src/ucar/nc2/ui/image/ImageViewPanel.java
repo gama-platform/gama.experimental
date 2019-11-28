04e91d24b757f612ca17a6a867d655fac05cca3c
hqnghi88 <hqnghi88@gmail.com>
2019-11-28 23:43:39 +0700

    reorganise netcdf

--- cict.gaml.extensions.netcdf/src/ucar/nc2/ui/image/ImageViewPanel.java
+++ cict.gaml.extensions.netcdf/src/ucar/nc2/ui/image/ImageViewPanel.java
@@ -33,127 +33,59 @@
 package ucar.nc2.ui.image;
 
 import java.awt.*;
-import java.awt.event.*;
 import java.awt.image.*;
-import java.net.URL;
-import java.net.MalformedURLException;
-import java.io.File;
-import java.util.Random;
 import javax.swing.*;
 import javax.swing.event.*;
 
-import ucar.nc2.dt.image.ImageDatasetFactory;
-import ucar.nc2.dt.image.ImageFactoryRandom;
-import ucar.nc2.dt.GridDatatype;
-
 /**
- *  *
+ * *
+ * 
  * @author caron
  */
 public class ImageViewPanel extends JPanel {
-  private static boolean debug = false;
 
-  public ImageDatasetFactory imageDatasetFactory = new ImageDatasetFactory();
-  private javax.swing.Timer timer;
-  private int delay = 4000; // millisescs
-  private JSpinner spinner;
-  private PicturePane pixPane;
+	private PicturePane pixPane;
 
-  public ImageViewPanel(Container buttPanel) {
-    pixPane = new PicturePane();
-    setLayout(new BorderLayout());
-    add( pixPane, BorderLayout.CENTER);
+	public ImageViewPanel(Container buttPanel) {
+		pixPane = new PicturePane();
+		setLayout(new BorderLayout());
+		add(pixPane, BorderLayout.CENTER);
 
-    if (buttPanel == null) {
-      buttPanel = new JPanel();
-      add( buttPanel, BorderLayout.NORTH);
-    }
+		if (buttPanel == null) {
+			buttPanel = new JPanel();
+			add(buttPanel, BorderLayout.NORTH);
+		}
 
-    spinner = new JSpinner( new SpinnerNumberModel(5000, 10, 20000, 1000));
-    spinner.addChangeListener( new ChangeListener() {
-      public void stateChanged(ChangeEvent e) {
-        Integer value = (Integer) spinner.getModel().getValue();
-        delay = value.intValue();
-        if (timer != null) timer.setDelay( delay);
-      }
-    });
-    buttPanel.add( spinner);
-  }
+	}
 
-  public void setImageFromGrid( GridDatatype grid) {
-      try {
-        BufferedImage image = imageDatasetFactory.openDataset( grid);
-        setImage( image);
+	public void setImage(BufferedImage im) {
+		if (im == null)
+			return; 
+		pixPane.setBufferedImage(im, "setImage");
+		pixPane.repaint();
+	}
 
-      } catch (Exception e2) {
-        javax.swing.JOptionPane.showMessageDialog(null, "Error on dataset\n"+ imageDatasetFactory.getErrorMessages());
-        e2.printStackTrace();
-      }
-  }
-
-
-   public boolean setImageFromUrl( String location) {
-     if (location.startsWith("http")) {
-       URL url = null;
-       try {
-         url = new URL( location);
-       } catch (MalformedURLException e) {
-         e.printStackTrace();
-       }
-
-       // uses ImageIO.createImageInputStream()
-       pixPane.setPicture( url, "legendParam", 0.0);
-
-     } else {
-
-      try {
-        BufferedImage image = imageDatasetFactory.open( location);
-        if (image == null) {
-          javax.swing.JOptionPane.showMessageDialog(null, "Cant open dataset as image = "+location+"\n"+ imageDatasetFactory.getErrorMessages());
-          return false;
-        }
-
-        setImage( image);
-
-      } catch (Exception e2) {
-        javax.swing.JOptionPane.showMessageDialog(null, "Error on dataset = "+location+"\n"+ imageDatasetFactory.getErrorMessages());
-        e2.printStackTrace();
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  public void setImage( BufferedImage im) {
-    if (im == null) return;
-    if (debug) System.out.println("ImageViewPanel setImage ");
-    pixPane.setBufferedImage( im, "setImage");
-    pixPane.repaint();
-  }
-
-  static public void main( String[] args) {
-      String[] readers = javax.imageio.ImageIO.getReaderFormatNames();
-      for (int i = 0; i < readers.length; i++) {
-        System.out.println(" reader = "+ readers[i]);
-      }
-      System.out.println("-------------------------------------------");
-      String[] readerMimes = javax.imageio.ImageIO.getReaderMIMETypes();
-      for (int i = 0; i < readerMimes.length; i++) {
-        System.out.println(" readerMimes = "+ readerMimes[i]);
-      }
-      System.out.println("-------------------------------------------");
-      String[] writers = javax.imageio.ImageIO.getWriterFormatNames();
-      for (int i = 0; i < writers.length; i++) {
-        System.out.println(" writers = "+ writers[i]);
-      }
-      System.out.println("-------------------------------------------");
-      String[] writerMimes = javax.imageio.ImageIO.getWriterMIMETypes();
-      for (int i = 0; i < writerMimes.length; i++) {
-        System.out.println(" writerMimes = "+ writerMimes[i]);
-      }
-      System.out.println("-------------------------------------------");
-  }
+	static public void main(String[] args) {
+		String[] readers = javax.imageio.ImageIO.getReaderFormatNames();
+		for (int i = 0; i < readers.length; i++) {
+			System.out.println(" reader = " + readers[i]);
+		}
+		System.out.println("-------------------------------------------");
+		String[] readerMimes = javax.imageio.ImageIO.getReaderMIMETypes();
+		for (int i = 0; i < readerMimes.length; i++) {
+			System.out.println(" readerMimes = " + readerMimes[i]);
+		}
+		System.out.println("-------------------------------------------");
+		String[] writers = javax.imageio.ImageIO.getWriterFormatNames();
+		for (int i = 0; i < writers.length; i++) {
+			System.out.println(" writers = " + writers[i]);
+		}
+		System.out.println("-------------------------------------------");
+		String[] writerMimes = javax.imageio.ImageIO.getWriterMIMETypes();
+		for (int i = 0; i < writerMimes.length; i++) {
+			System.out.println(" writerMimes = " + writerMimes[i]);
+		}
+		System.out.println("-------------------------------------------");
+	}
 
 }
-
