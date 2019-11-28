b2df1691195ec5a76282cb6e5177f96ffc23bc99
hqnghi88 <hqnghi88@gmail.com>
2019-11-28 23:43:39 +0700

    reorganise netcdf

--- cict.gaml.extensions.netcdf/src/cict/gaml/extensions/netcdf/file/TestNetCDF.java
+++ cict.gaml.extensions.netcdf/src/cict/gaml/extensions/netcdf/file/TestNetCDF.java
@@ -4,10 +4,8 @@ import java.awt.Rectangle;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.util.Formatter;
 import java.util.List;
 
@@ -15,23 +13,36 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+import ucar.ma2.Array;
 import ucar.nc2.dataset.NetcdfDataset;
+import ucar.nc2.dt.GridCoordSystem;
 import ucar.nc2.dt.GridDataset;
 import ucar.nc2.dt.GridDatatype;
+import ucar.nc2.dt.image.ImageArrayAdapter;
 import ucar.nc2.ui.image.ImageViewPanel;
 
 public class TestNetCDF extends JPanel {
 
-	private static TestNetCDF ui;
+//	String netCDF_File = "E:/ROMS/SENEGAL_Y1980M1.nc";
+	String netCDF_File = "E:/tos_O1_2001-2002.nc";
+//	String netCDF_File = "E:/test_echam_spectral.nc";
+//	String netCDF_File = "E:/cami_0000-09-01_64x128_L26_c030918.nc";
+//	String netCDF_File = "C:\\git\\gama.experimental\\cict.gaml.extensions.netcdf\\models\\NetCDF\\includes\\tos_O1_2001-2002.nc";
+//	String netCDF_File = "E:/sresa1b_ncar_ccsm3-example.nc";
+	private static TestNetCDF tester;
 	private static JFrame frame;
 	private static ImageViewPanel imgpan;
 	NetcdfDataset ds = null;
 	private GridDataset gridDataset;
+	private int nbgrid = 0;
+	private int time = 0;
+	private int ntimes = 1;
+	boolean forward = true;
 
 	public static void main(final String args[]) throws Exception {
-		frame = new JFrame("NetCDF (4.2) Tools");
+		frame = new JFrame("NetCDF test");
 		imgpan = new ImageViewPanel(frame);
-		ui = new TestNetCDF();
+		tester = new TestNetCDF();
 		frame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
@@ -43,13 +54,34 @@ public class TestNetCDF extends JPanel {
 		frame.setBounds(new Rectangle(50, 50, 800, 450));
 		frame.setVisible(true);
 
-		ui.process();
+		tester.process();
 		new Thread() {
 
 			@Override
 			public void run() {
-				BufferedImage img = imgpan.imageDatasetFactory.getNextImage(true);
-				while (img != null) {
+				BufferedImage img = null;
+				do {
+
+					if (tester.forward) {
+						tester.time++;
+						if (tester.time >= tester.ntimes) {
+							tester.nbgrid++;
+							tester.time = 0;
+						}
+					} else {
+						tester.time--;
+						if (tester.time < 0)
+							tester.time = tester.ntimes - 1;
+					}
+
+					Array data;
+					try {
+						data = tester.getGrid().readDataSlice(tester.time, 0, -1, -1);
+						img = ImageArrayAdapter.makeGrayscaleImage(data, null);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+
 					imgpan.setImage(img);
 					try {
 						Thread.sleep(100);
@@ -57,41 +89,32 @@ public class TestNetCDF extends JPanel {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-					img = imgpan.imageDatasetFactory.getNextImage(true);
-				}
+				} while (img != null);
 			}
 
 		}.start();
 	}
 
 	boolean process() {
-
-//		String command = "E:/ROMS/SENEGAL_Y1980M1.nc";
-		String command = "E:/tos_O1_2001-2002.nc";
-//		String command = "E:/test_echam_spectral.nc";
-//		String command = "E:/cami_0000-09-01_64x128_L26_c030918.nc";
-//		String command = "C:\\git\\gama.experimental\\cict.gaml.extensions.netcdf\\models\\NetCDF\\includes\\tos_O1_2001-2002.nc";
-//		String command = "E:/sresa1b_ncar_ccsm3-example.nc";
 		boolean err = false;
 
-		NetcdfDataset newds;
-		ByteArrayOutputStream bos = new ByteArrayOutputStream(10000);
 		try {
-			newds = NetcdfDataset.openDataset(command, true, null);
-			if (newds == null) {
-				JOptionPane.showMessageDialog(null, "NetcdfDataset.open cant open " + command);
+			this.ds = NetcdfDataset.openDataset(netCDF_File, true, null);
+			if (this.ds == null) {
+				JOptionPane.showMessageDialog(null, "NetcdfDataset.open cant open " + netCDF_File);
 				return false;
 			}
-			setDataset(newds);
+
+			this.gridDataset = new ucar.nc2.dt.grid.GridDataset(this.ds, new Formatter());
 
 		} catch (FileNotFoundException ioe) {
-			JOptionPane.showMessageDialog(null, "NetcdfDataset.open cant open " + command + "\n" + ioe.getMessage());
+			JOptionPane.showMessageDialog(null,
+					"NetcdfDataset.open cant open " + netCDF_File + "\n" + ioe.getMessage());
 			ioe.printStackTrace();
 			err = true;
 
 		} catch (Throwable ioe) {
 			ioe.printStackTrace();
-			ioe.printStackTrace(new PrintStream(bos));
 			err = true;
 		}
 
@@ -99,7 +122,18 @@ public class TestNetCDF extends JPanel {
 			GridDatatype grid = getGrid();
 			if (grid == null)
 				return false;
-			imgpan.setImageFromGrid(grid);
+			this.time = 0;
+			GridCoordSystem gcsys = grid.getCoordinateSystem();
+			if (gcsys.getTimeAxis() != null)
+				ntimes = (int) gcsys.getTimeAxis().getSize();
+
+			Array data;
+			try {
+				data = grid.readDataSlice(this.time, 0, -1, -1);
+				imgpan.setImage(ImageArrayAdapter.makeGrayscaleImage(data, null));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		return !err;
 
@@ -109,31 +143,35 @@ public class TestNetCDF extends JPanel {
 //	    GeogridBean vb = (GeogridBean) varTable.getSelectedBean();
 //	    if (vb == null) {
 		List<?> grids = gridDataset.getGrids();
+//		System.out.println(grids.size());
+		if (nbgrid >= grids.size()) {
+			nbgrid = 0;
+		}
 		if (grids.size() > 0)
-			return (GridDatatype) grids.get(0);
+			return (GridDatatype) grids.get(nbgrid);// TODO number of the map
 		else
 			return null;
 //	    }
 //	    return gridDataset.findGridDatatype( vb.getName());
 	}
 
-	void setDataset(NetcdfDataset newds) {
-		if (newds == null)
-			return;
-		try {
-			if (ds != null)
-				ds.close();
-		} catch (IOException ioe) {
-		}
-
-		Formatter parseInfo = new Formatter();
-		this.ds = newds;
-		try {
-
-			this.gridDataset = new ucar.nc2.dt.grid.GridDataset(ds, parseInfo);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
-		}
-	}
+//	void setDataset(NetcdfDataset newds) {
+//		if (newds == null)
+//			return;
+//		try {
+//			if (ds != null)
+//				ds.close();
+//		} catch (IOException ioe) {
+//		}
+//
+//		Formatter parseInfo = new Formatter();
+//		this.ds = newds;
+//		try {
+//
+//			this.gridDataset = new ucar.nc2.dt.grid.GridDataset(ds, parseInfo);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			return;
+//		}
+//	}
 }
