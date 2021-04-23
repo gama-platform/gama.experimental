cb00660f623d20e443d80507eeeaa3c3f044e176
HUYNH Nghi <hqnghi88@gmail.com>
2021-04-24 01:10:13 +0700

    websocket web display preliminary

--- cict.gama.extensions.websocket/src/cict/gama/extensions/websocket/WebDisplaySurface.java
+++ cict.gama.extensions.websocket/src/cict/gama/extensions/websocket/WebDisplaySurface.java
@@ -13,40 +13,95 @@ package cict.gama.extensions.websocket;
 
 import java.awt.Point;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.UnknownHostException;
 import java.util.Collection;
 
-import org.java_websocket.WebSocket;
-import org.java_websocket.handshake.ClientHandshake;
-import org.java_websocket.server.WebSocketServer;
 import org.locationtech.jts.geom.Envelope;
 
 import msi.gama.common.interfaces.IDisplaySurface;
+import msi.gama.common.interfaces.IGraphics;
+import msi.gama.common.interfaces.IKeyword;
 import msi.gama.common.interfaces.ILayer;
 import msi.gama.common.interfaces.ILayerManager;
-import msi.gama.common.preferences.GamaPreferences;
 import msi.gama.metamodel.agent.IAgent;
 import msi.gama.metamodel.shape.ILocation;
 import msi.gama.metamodel.shape.IShape;
 import msi.gama.outputs.LayeredDisplayData;
 import msi.gama.outputs.LayeredDisplayData.Changes;
 import msi.gama.outputs.LayeredDisplayOutput;
+import msi.gama.outputs.display.LayerManager;
 import msi.gama.outputs.layers.IEventLayerListener;
 import msi.gama.precompiler.GamlAnnotations.display;
 import msi.gama.precompiler.GamlAnnotations.doc;
+import msi.gama.runtime.GAMA;
 import msi.gama.runtime.IScope;
-import ummisco.gama.dev.utils.DEBUG; 
+import msi.gama.util.IList;
+import msi.gaml.expressions.IExpression;
+import ummisco.gama.ui.views.displays.DisplaySurfaceMenu;
 
-@display ("web")
-@doc ("Display that uses the websocket technology to draw the layers in a web view")
-public class WebDisplaySurface extends WebSocketServer implements IDisplaySurface {
- 
-	public WebDisplaySurface(final Object... args) { 
+@display("web")
+@doc("Display that uses the websocket technology to draw the layers in a web view")
+public class WebDisplaySurface implements IDisplaySurface {
+
+	final LayeredDisplayOutput output;
+
+	private IScope scope;
+	protected final ILayerManager layerManager;
+	protected IGraphics iGraphics;
+
+	protected DisplaySurfaceMenu menuManager;
+	protected IExpression temp_focus;
+
+	public WebDisplaySurface(final Object... args) {
+		output = (LayeredDisplayOutput) args[0];
+		output.setSurface(this);
+		setDisplayScope(output.getScope().copy("in java2D display"));
+		output.getData().addListener(this);
+		temp_focus = output.getFacet(IKeyword.FOCUS);
+		layerManager = new LayerManager(this, output);
+		int port = 8887; // 843 flash policy port
+
+		try {
+			final ChatServer s = new ChatServer(port);
+			s.start();
+			System.out.println("ChatServer started on port: " + s.getPort());
+			scope.getSimulation().postDisposeAction(scope1 -> {
+				try {
+					s.stop();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return null;
+			});
+//			BufferedReader sysin = new BufferedReader(new InputStreamReader(System.in));
+//			while (true) {
+//				String in = sysin.readLine();
+//				s.broadcast(in);
+//				if (in.equals("exit")) {
+//					s.stop(1000);
+//					break;
+//				}
+//			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	protected void setDisplayScope(final IScope scope) {
+		if (this.scope != null) {
+			GAMA.releaseScope(this.scope);
+		}
+		this.scope = scope;
 	}
 
 	@Override
 	public void changed(Changes property, Object value) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -64,49 +119,49 @@ public class WebDisplaySurface extends WebSocketServer implements IDisplaySurfac
 	@Override
 	public void updateDisplay(boolean force) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setMenuManager(Object displaySurfaceMenu) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void zoomIn() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void zoomOut() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void zoomFit() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public ILayerManager getManager() {
 		// TODO Auto-generated method stub
-		return null;
+		return layerManager;
 	}
 
 	@Override
 	public void focusOn(IShape geometry) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void runAndUpdate(Runnable r) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -124,7 +179,7 @@ public class WebDisplaySurface extends WebSocketServer implements IDisplaySurfac
 	@Override
 	public void outputReloaded() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -172,7 +227,7 @@ public class WebDisplaySurface extends WebSocketServer implements IDisplaySurfac
 	@Override
 	public void followAgent(IAgent a) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -184,7 +239,7 @@ public class WebDisplaySurface extends WebSocketServer implements IDisplaySurfac
 	@Override
 	public void setSize(int x, int y) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -195,26 +250,25 @@ public class WebDisplaySurface extends WebSocketServer implements IDisplaySurfac
 
 	@Override
 	public LayeredDisplayData getData() {
-		// TODO Auto-generated method stub
-		return null;
+		return output.getData();
 	}
 
 	@Override
 	public void layersChanged() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void addListener(IEventLayerListener e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void removeListener(IEventLayerListener e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -256,69 +310,36 @@ public class WebDisplaySurface extends WebSocketServer implements IDisplaySurfac
 	@Override
 	public void getModelCoordinatesInfo(StringBuilder receiver) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void dispatchKeyEvent(char character) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void dispatchMouseEvent(int swtEventType) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setMousePosition(int x, int y) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void draggedTo(int x, int y) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void selectAgentsAroundMouse() {
 		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void onClose(WebSocket arg0, int arg1, String arg2, boolean arg3) {
-		// TODO Auto-generated method stub
-		
 	}
-
-	@Override
-	public void onError(WebSocket arg0, Exception arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onMessage(WebSocket arg0, String arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onOpen(WebSocket arg0, ClientHandshake arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onStart() {
-		// TODO Auto-generated method stub
-		
-	}
- 
- 
-
 }
