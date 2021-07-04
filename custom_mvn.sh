59733edabf5f443011adf39a554c002d325b024c
hqnghi88 <hqnghi88@gmail.com>
2021-07-04 10:22:32 +0700

    retry with replacing setting

--- custom_mvn.sh
+++ custom_mvn.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 
-search='</settings>'
-replace='<mirror><id>insecure-repo</id><mirrorOf>external:http:*</mirrorOf><url>http://51.255.46.42/updates</url><blocked>false</blocked></mirror></settings>'
-sed -i "s/<\/settings>/<mirror><id>insecure-repo<\/id><mirrorOf>external:http:*<\/mirrorOf><url>http:\/\/51.255.46.42\/updates<\/url><blocked>false<\/blocked><\/mirror><\/settings>/" settings.xml
+ 
+sed -i "s/<\/settings>/<mirror><id>insecure-repo<\/id><mirrorOf>p2Repo<\/mirrorOf><url>http:\/\/51.255.46.42\/updates<\/url><blocked>false<\/blocked><\/mirror><\/settings>/" ~/.m2/settings.xml
