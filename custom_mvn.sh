eae6c13dae2c7e9cb5f24dc5341b9f099a247ade
hqnghi88 <hqnghi88@gmail.com>
2021-07-04 12:32:46 +0700

    cat conf settings

--- custom_mvn.sh
+++ custom_mvn.sh
@@ -2,4 +2,4 @@
 
 
  
-sed -i "s/<\/settings>/<mirror><id>insecure-repo<\/id><mirrorOf>p2Repo<\/mirrorOf><url>http:\/\/51.255.46.42\/updates<\/url><blocked>false<\/blocked><\/mirror><\/settings>/" ~/.m2/settings.xml
+sed -i "s/<\/settings>/<mirrors><mirror><id>insecure-repo<\/id><mirrorOf>p2Repo<\/mirrorOf><url>http:\/\/51.255.46.42\/updates<\/url><blocked>false<\/blocked><\/mirror><\/mirrors><\/settings>/" ~/.m2/settings.xml
