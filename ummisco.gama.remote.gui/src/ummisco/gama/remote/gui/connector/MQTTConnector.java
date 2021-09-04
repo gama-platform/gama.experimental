4dae55f3c0b61425ba002a1070e0a75da37cc9b3
hqnghi88 <hqnghi88@gmail.com>
2021-09-04 20:36:22 +0700

    ci deploy temporary disbale remotegui content

--- ummisco.gama.remote.gui/src/ummisco/gama/remote/gui/connector/MQTTConnector.java
+++ ummisco.gama.remote.gui/src/ummisco/gama/remote/gui/connector/MQTTConnector.java
@@ -4,132 +4,132 @@ import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+//import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+//import org.eclipse.paho.client.mqttv3.MqttCallback;
+//import org.eclipse.paho.client.mqttv3.MqttClient;
+//import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+//import org.eclipse.paho.client.mqttv3.MqttException;
+//import org.eclipse.paho.client.mqttv3.MqttMessage;
+//import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
 public final class MQTTConnector {
-	public final static String SERVER_URL = "SERVER_URL";
-	public final static String SERVER_PORT = "SERVER_PORT";
-	public final static String LOCAL_NAME = "LOCAL_NAME";
-	public final static String LOGIN = "LOGIN";
-	public final static String PASSWORD = "PASSWORD";
-
-	public static String DEFAULT_USER = "admin";
-	public static String DEFAULT_LOCAL_NAME = "gama-ui" + Calendar.getInstance().getTimeInMillis() + "@";
-	public static String DEFAULT_PASSWORD = "password";
-	public static String DEFAULT_HOST = "localhost";
-	public static String DEFAULT_PORT = "1883";
-
-	protected MqttClient sendConnection = null;
-	Map<String, Object> receivedData;
-
-	public MQTTConnector(final String server, final String userName, final String password) throws MqttException {
-		this.connectToServer(server, null, userName, password);
-		receivedData = new HashMap<>();
-	}
-
-	class Callback implements MqttCallback {
-		@Override
-		public void connectionLost(final Throwable arg0) {
-			// throw new MqttException(arg0);
-			System.out.println("connection lost");
-		}
-
-		@Override
-		public void deliveryComplete(final IMqttDeliveryToken arg0) {
-			System.out.println("message sended");
-		}
-
-		@Override
-		public void messageArrived(final String topic, final MqttMessage message) throws Exception {
-			final String body = message.toString();
-			storeData(topic, body);
-		}
-	}
-
-	public Object getLastData(final String topic) {
-		final Object data = storeDataS(topic, null);
-		return data;
-	}
-
-	private synchronized Object storeDataS(final String topic, final Object dts) {
-		if (dts == null) {
-			final Object tmp = this.receivedData.get(topic);
-			this.receivedData.remove(topic);
-			return tmp;
-
-		}
-		this.receivedData.remove(topic);
-		this.receivedData.put(topic, dts);
-		return dts;
-	}
-	
-	class Data{
-			public Object name;
-			public Object value;
-	}
-	
-	private void storeData(final String topic, final String message) {
-		final XStream dataStreamer = new XStream(new DomDriver());
-		final Object data = dataStreamer.fromXML(message);
-	
-		System.out.println(" Received message is : "+ message);
-		System.out.println(" Received Data is : "+ data);
-	
-		storeDataS(topic, data);
-	}
-
-	public void releaseConnection() throws MqttException {
-		sendConnection.disconnect();
-		sendConnection = null;
-	}
-
-	public void sendMessage(final String dest, final Object data) throws MqttException {
-		final XStream dataStreamer = new XStream(new DomDriver());
-		final String dataS = dataStreamer.toXML(data);
-		this.sendFormatedMessage(dest, dataS);
-	}
-
-	private void sendFormatedMessage(final String receiver, final String content) throws MqttException {
-		final MqttMessage mm = new MqttMessage(content.getBytes());
-		sendConnection.publish(receiver, mm);
-		System.out.println(" ---> Content is : "+content);
-	}
-
-	public void subscribeToGroup(final String boxName) throws MqttException {
-		sendConnection.subscribe(boxName);
-	}
-
-	public void unsubscribeGroup(final String boxName) throws MqttException {
-		sendConnection.unsubscribe(boxName);
-	}
-
-	protected void connectToServer(final String originalServer, final String originalPort,
-			final String originalUserName, final String originalPassword) throws MqttException {
-		if (sendConnection == null) {
-			final String server = originalServer == null ? DEFAULT_HOST : originalServer;
-			final String port = originalPort == null ? DEFAULT_PORT : originalPort;
-			final String userName = originalUserName == null ? DEFAULT_USER : originalUserName;
-			final String password = originalPassword == null ? DEFAULT_PASSWORD : originalPassword;
-			final String localName = DEFAULT_LOCAL_NAME + server;
-			sendConnection = new MqttClient("tcp://" + server + ":" + port, localName, new MemoryPersistence());
-			final MqttConnectOptions connOpts = new MqttConnectOptions();
-			connOpts.setCleanSession(true);
-			sendConnection.setCallback(new Callback());
-			connOpts.setCleanSession(true);
-			connOpts.setKeepAliveInterval(30);
-			connOpts.setUserName(userName);
-			connOpts.setPassword(password.toCharArray());
-			sendConnection.connect(connOpts);
-
-		}
-	}
+//	public final static String SERVER_URL = "SERVER_URL";
+//	public final static String SERVER_PORT = "SERVER_PORT";
+//	public final static String LOCAL_NAME = "LOCAL_NAME";
+//	public final static String LOGIN = "LOGIN";
+//	public final static String PASSWORD = "PASSWORD";
+//
+//	public static String DEFAULT_USER = "admin";
+//	public static String DEFAULT_LOCAL_NAME = "gama-ui" + Calendar.getInstance().getTimeInMillis() + "@";
+//	public static String DEFAULT_PASSWORD = "password";
+//	public static String DEFAULT_HOST = "localhost";
+//	public static String DEFAULT_PORT = "1883";
+//
+//	protected MqttClient sendConnection = null;
+//	Map<String, Object> receivedData;
+//
+//	public MQTTConnector(final String server, final String userName, final String password) throws MqttException {
+//		this.connectToServer(server, null, userName, password);
+//		receivedData = new HashMap<>();
+//	}
+//
+//	class Callback implements MqttCallback {
+//		@Override
+//		public void connectionLost(final Throwable arg0) {
+//			// throw new MqttException(arg0);
+//			System.out.println("connection lost");
+//		}
+//
+//		@Override
+//		public void deliveryComplete(final IMqttDeliveryToken arg0) {
+//			System.out.println("message sended");
+//		}
+//
+//		@Override
+//		public void messageArrived(final String topic, final MqttMessage message) throws Exception {
+//			final String body = message.toString();
+//			storeData(topic, body);
+//		}
+//	}
+//
+//	public Object getLastData(final String topic) {
+//		final Object data = storeDataS(topic, null);
+//		return data;
+//	}
+//
+//	private synchronized Object storeDataS(final String topic, final Object dts) {
+//		if (dts == null) {
+//			final Object tmp = this.receivedData.get(topic);
+//			this.receivedData.remove(topic);
+//			return tmp;
+//
+//		}
+//		this.receivedData.remove(topic);
+//		this.receivedData.put(topic, dts);
+//		return dts;
+//	}
+//	
+//	class Data{
+//			public Object name;
+//			public Object value;
+//	}
+//	
+//	private void storeData(final String topic, final String message) {
+//		final XStream dataStreamer = new XStream(new DomDriver());
+//		final Object data = dataStreamer.fromXML(message);
+//	
+//		System.out.println(" Received message is : "+ message);
+//		System.out.println(" Received Data is : "+ data);
+//	
+//		storeDataS(topic, data);
+//	}
+//
+//	public void releaseConnection() throws MqttException {
+//		sendConnection.disconnect();
+//		sendConnection = null;
+//	}
+//
+//	public void sendMessage(final String dest, final Object data) throws MqttException {
+//		final XStream dataStreamer = new XStream(new DomDriver());
+//		final String dataS = dataStreamer.toXML(data);
+//		this.sendFormatedMessage(dest, dataS);
+//	}
+//
+//	private void sendFormatedMessage(final String receiver, final String content) throws MqttException {
+//		final MqttMessage mm = new MqttMessage(content.getBytes());
+//		sendConnection.publish(receiver, mm);
+//		System.out.println(" ---> Content is : "+content);
+//	}
+//
+//	public void subscribeToGroup(final String boxName) throws MqttException {
+//		sendConnection.subscribe(boxName);
+//	}
+//
+//	public void unsubscribeGroup(final String boxName) throws MqttException {
+//		sendConnection.unsubscribe(boxName);
+//	}
+//
+//	protected void connectToServer(final String originalServer, final String originalPort,
+//			final String originalUserName, final String originalPassword) throws MqttException {
+//		if (sendConnection == null) {
+//			final String server = originalServer == null ? DEFAULT_HOST : originalServer;
+//			final String port = originalPort == null ? DEFAULT_PORT : originalPort;
+//			final String userName = originalUserName == null ? DEFAULT_USER : originalUserName;
+//			final String password = originalPassword == null ? DEFAULT_PASSWORD : originalPassword;
+//			final String localName = DEFAULT_LOCAL_NAME + server;
+//			sendConnection = new MqttClient("tcp://" + server + ":" + port, localName, new MemoryPersistence());
+//			final MqttConnectOptions connOpts = new MqttConnectOptions();
+//			connOpts.setCleanSession(true);
+//			sendConnection.setCallback(new Callback());
+//			connOpts.setCleanSession(true);
+//			connOpts.setKeepAliveInterval(30);
+//			connOpts.setUserName(userName);
+//			connOpts.setPassword(password.toCharArray());
+//			sendConnection.connect(connOpts);
+//
+//		}
+//	}
 }
--- ummisco.gama.remote.gui/src/ummisco/gama/remote/gui/skill/SharedVariable.java
+++ ummisco.gama.remote.gui/src/ummisco/gama/remote/gui/skill/SharedVariable.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.eclipse.paho.client.mqttv3.MqttException;
+//import org.eclipse.paho.client.mqttv3.MqttException;
 
 import msi.gama.metamodel.agent.IAgent;
 import msi.gama.runtime.IScope;
@@ -14,99 +14,99 @@ import ummisco.gama.remote.gui.connector.MQTTConnector;
 import ummisco.gama.remote.reducer.DataReducer;
 
 public class SharedVariable {
-	public final static int EXPOSED_VARIABLE = 1;
-	public final static int LISTENED_VARIABLE = 2;
-
-	ArrayList<String> attributeName;
-	String exposedName;
-
-	int connectionType = -1;
-	Object value;
-	int lastUpdate;
-	MQTTConnector connection;
-	IAgent agent;
-
-	SharedVariable(final IAgent agt, final String names, final String exposedName, final MQTTConnector connect,
-			final int IO) throws MqttException {
-		connection = connect; // new MQTTConnector(server,login,pass,exposedName,IO);
-		this.agent = agt;
-		this.attributeName = new ArrayList<>();
-		this.attributeName.add(names);
-		this.exposedName = exposedName;
-		this.connectionType = IO;
-		if (connectionType == LISTENED_VARIABLE) {
-			connection.subscribeToGroup(exposedName);
-		}
-		this.update(agt.getScope());
-	}
-
-	SharedVariable(final IAgent agt, final ArrayList<String> names, final String exposedName,
-			final MQTTConnector connect, final int IO) throws MqttException {
-		connection = connect; // new MQTTConnector(server,login,pass,exposedName,IO);
-		this.agent = agt;
-		this.attributeName = names;
-		this.exposedName = exposedName;
-		this.connectionType = IO;
-		if (connectionType == LISTENED_VARIABLE) {
-			connection.subscribeToGroup(exposedName);
-		}
-		this.update(agt.getScope());
-	}
-
-	public void update(final IScope scope) {
-		switch (connectionType) {
-			case EXPOSED_VARIABLE: {
-				exposeValue();
-				break;
-			}
-			case LISTENED_VARIABLE: {
-				listenValue();
-				break;
-			}
-		}
-	}
-
-	public void dispose() {
-
-	}
-
-	private boolean attributeChanged() {
-		for (final String cn : this.attributeName) {
-			System.out.println(
-					"test equ " + agent.getAttribute(cn) + "  " + value + "  " + agent.getAttribute(cn).equals(value));
-			if (!agent.getAttribute(cn).equals(value)) { return true; }
-		}
-		return false;
-	}
-
-	private void exposeValue() {
-		try {
-			final Map<String, Object> mmap = new HashMap<>();
-			for (final String cn : this.attributeName) {
-				Object data = agent.getAttribute(cn);
-				if (data instanceof IList) {
-					data = DataReducer.castToList((IList<?>) data);
-				}
-				if (data instanceof IMap) {
-					data = DataReducer.castToMap((IMap<?, ?>) data);
-				}
-				mmap.put(cn, data);
-				this.value = data;
-			}
-			connection.sendMessage(exposedName, mmap);
-		} catch (final MqttException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	private void listenValue() {
-		final Object data = connection.getLastData(exposedName);
-		if (data != null) {
-			this.value = data;
-			//this.agent.setAttribute(attributeName.get(0), data);
-			this.agent.setAttribute(attributeName.get(0),  ((Map<String, Object>) data).get(attributeName.get(0)));
-		}
-	}
+//	public final static int EXPOSED_VARIABLE = 1;
+//	public final static int LISTENED_VARIABLE = 2;
+//
+//	ArrayList<String> attributeName;
+//	String exposedName;
+//
+//	int connectionType = -1;
+//	Object value;
+//	int lastUpdate;
+//	MQTTConnector connection;
+//	IAgent agent;
+//
+//	SharedVariable(final IAgent agt, final String names, final String exposedName, final MQTTConnector connect,
+//			final int IO) throws MqttException {
+//		connection = connect; // new MQTTConnector(server,login,pass,exposedName,IO);
+//		this.agent = agt;
+//		this.attributeName = new ArrayList<>();
+//		this.attributeName.add(names);
+//		this.exposedName = exposedName;
+//		this.connectionType = IO;
+//		if (connectionType == LISTENED_VARIABLE) {
+//			connection.subscribeToGroup(exposedName);
+//		}
+//		this.update(agt.getScope());
+//	}
+//
+//	SharedVariable(final IAgent agt, final ArrayList<String> names, final String exposedName,
+//			final MQTTConnector connect, final int IO) throws MqttException {
+//		connection = connect; // new MQTTConnector(server,login,pass,exposedName,IO);
+//		this.agent = agt;
+//		this.attributeName = names;
+//		this.exposedName = exposedName;
+//		this.connectionType = IO;
+//		if (connectionType == LISTENED_VARIABLE) {
+//			connection.subscribeToGroup(exposedName);
+//		}
+//		this.update(agt.getScope());
+//	}
+//
+//	public void update(final IScope scope) {
+//		switch (connectionType) {
+//			case EXPOSED_VARIABLE: {
+//				exposeValue();
+//				break;
+//			}
+//			case LISTENED_VARIABLE: {
+//				listenValue();
+//				break;
+//			}
+//		}
+//	}
+//
+//	public void dispose() {
+//
+//	}
+//
+//	private boolean attributeChanged() {
+//		for (final String cn : this.attributeName) {
+//			System.out.println(
+//					"test equ " + agent.getAttribute(cn) + "  " + value + "  " + agent.getAttribute(cn).equals(value));
+//			if (!agent.getAttribute(cn).equals(value)) { return true; }
+//		}
+//		return false;
+//	}
+//
+//	private void exposeValue() {
+//		try {
+//			final Map<String, Object> mmap = new HashMap<>();
+//			for (final String cn : this.attributeName) {
+//				Object data = agent.getAttribute(cn);
+//				if (data instanceof IList) {
+//					data = DataReducer.castToList((IList<?>) data);
+//				}
+//				if (data instanceof IMap) {
+//					data = DataReducer.castToMap((IMap<?, ?>) data);
+//				}
+//				mmap.put(cn, data);
+//				this.value = data;
+//			}
+//			connection.sendMessage(exposedName, mmap);
+//		} catch (final MqttException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	private void listenValue() {
+//		final Object data = connection.getLastData(exposedName);
+//		if (data != null) {
+//			this.value = data;
+//			//this.agent.setAttribute(attributeName.get(0), data);
+//			this.agent.setAttribute(attributeName.get(0),  ((Map<String, Object>) data).get(attributeName.get(0)));
+//		}
+//	}
 
 }
