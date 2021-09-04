2b9f75b090ebccb31f6ac0007dafecda8d919ec0
hqnghi88 <hqnghi88@gmail.com>
2021-09-04 16:45:05 +0700

    ci deploy reconfig the decrypt of settings.xml

--- publish.sh
+++ publish.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
 deploy(){	
+
+bash $GITHUB_WORKSPACE/decrypt_secret.sh
+
+header=$(<$GITHUB_WORKSPACE/settings_header.xml)
+core=$(<$GITHUB_WORKSPACE/settings_auth.xml)
+footer=$(<$GITHUB_WORKSPACE/settings_footer.xml) 
+echo "$header $core $footer"> $GITHUB_WORKSPACE/settings.xml
+
+
+echo "DEPLOY" 
+mkdir -m 0700 -p ~/.ssh 
+chmod 750 ~
+chmod 700 ~/.ssh 
+echo -e "Host *\n" >> ~/.ssh/config 
+echo -e "IdentitiesOnly yes\n" >> ~/.ssh/config 
+echo -e "Port 22\n" >> ~/.ssh/config 
+echo -e "StrictHostKeyChecking no\n" >> ~/.ssh/config 
+echo -e "PubkeyAuthentication no\n" >> ~/.ssh/config 
+cat ~/.ssh/config 
+ssh-keyscan -H $SSH_HOST >> ~/.ssh/known_hosts
+
 cd msi.gama.experimental.p2updatesite &&
 mvn -U clean install -P p2Repo --settings ../settings.xml && 
 cd -
